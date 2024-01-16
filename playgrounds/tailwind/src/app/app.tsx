7e5e737e07d2477bd29549814c5fcfbe40cf6c2f
Thorsten Hell <thorsten.hell@cismet.de>
2024-01-16 14:55:55 +0100

    added tailwind playground

---
+++ playgrounds/tailwind/src/app/app.tsx
@@ -0,0 +1,11 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import styles from './app.module.css';
+export function App() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  );
+}
+
+export default App;
