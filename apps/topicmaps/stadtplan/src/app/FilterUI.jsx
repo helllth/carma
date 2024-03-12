45e4d954f4feab68a9108068e10c438b50d3318f
David Glogaza <david.glogaza40@googlemail.com>
2024-03-12 14:28:13 +0100

    fix chartjs import

--- apps/topicmaps/stadtplan/src/app/FilterUI.jsx
+++ apps/topicmaps/stadtplan/src/app/FilterUI.jsx
@@ -1,34 +1,55 @@
-import { faThumbsDown, faThumbsUp } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import Chart from "chart.js";
-import React, { useEffect, useMemo, useState } from "react";
-import { useContext } from "react";
-import { Badge, Button, Form } from "react-bootstrap";
-import ReactChartkick, { PieChart } from "react-chartkick";
-import { FeatureCollectionContext, FeatureCollectionDispatchContext } from "react-cismap/contexts/FeatureCollectionContextProvider";
-import { ResponsiveTopicMapContext } from "react-cismap/contexts/ResponsiveTopicMapContextProvider";
-import { TopicMapStylingContext } from "react-cismap/contexts/TopicMapStylingContextProvider";
+import { faThumbsDown, faThumbsUp } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Chart } from 'chart.js';
+import React, { useEffect, useMemo, useState } from 'react';
+import { useContext } from 'react';
+import { Badge, Button, Form } from 'react-bootstrap';
+import ReactChartkick, { PieChart } from 'react-chartkick';
+import {
+  FeatureCollectionContext,
+  FeatureCollectionDispatchContext,
+} from 'react-cismap/contexts/FeatureCollectionContextProvider';
+import { ResponsiveTopicMapContext } from 'react-cismap/contexts/ResponsiveTopicMapContextProvider';
+import { TopicMapStylingContext } from 'react-cismap/contexts/TopicMapStylingContextProvider';
 
-import { crossLinkApps } from "./helper/constants";
-import { clearFilter, createFilterRows, setAllLebenslagenToFilter, toggleFilter } from "./helper/filter";
-import { getColorFromLebenslagenCombination } from "./helper/styler";
-import MultiToggleButton from "./MultiToggleButton";
+import { crossLinkApps } from './helper/constants';
+import {
+  clearFilter,
+  createFilterRows,
+  setAllLebenslagenToFilter,
+  toggleFilter,
+} from './helper/filter';
+import { getColorFromLebenslagenCombination } from './helper/styler';
+import MultiToggleButton from './MultiToggleButton';
 
-import "url-search-params-polyfill";
+import 'url-search-params-polyfill';
 
 ReactChartkick.addAdapter(Chart);
 
 const FilterUI = ({ apps = crossLinkApps }) => {
-  const { itemsDictionary, filteredItems, filterState } = useContext(FeatureCollectionContext);
+  const { itemsDictionary, filteredItems, filterState } = useContext(
+    FeatureCollectionContext
+  );
   const { setFilterState } = useContext(FeatureCollectionDispatchContext);
   const filteredPOIs = filteredItems || [];
-  const lebenslagen = useMemo(() => itemsDictionary?.lebenslagen || [], [itemsDictionary]);
+  const lebenslagen = useMemo(
+    () => itemsDictionary?.lebenslagen || [],
+    [itemsDictionary]
+  );
   const { windowSize } = useContext(ResponsiveTopicMapContext);
   const [filterRows, setFilterRows] = useState();
   const { additionalStylingInfo } = useContext(TopicMapStylingContext);
   const poiColors = additionalStylingInfo?.poiColors;
   useEffect(() => {
-    setFilterRows(createFilterRows(apps, lebenslagen, toggleFilter, filterState, setFilterState));
+    setFilterRows(
+      createFilterRows(
+        apps,
+        lebenslagen,
+        toggleFilter,
+        filterState,
+        setFilterState
+      )
+    );
   }, [apps, lebenslagen, setFilterState, filterState]);
 
   const width = windowSize?.width || 500;
@@ -42,7 +63,7 @@ const FilterUI = ({ apps = crossLinkApps }) => {
   let stats = {};
   let colormodel = {};
   for (let poi of filteredPOIs) {
-    if (stats[poi.mainlocationtype.lebenslagen.join(", ")] === undefined) {
+    if (stats[poi.mainlocationtype.lebenslagen.join(', ')] === undefined) {
       const ll = poi.mainlocationtype.lebenslagen;
 
       const key = ll
@@ -50,12 +71,12 @@ const FilterUI = ({ apps = crossLinkApps }) => {
         .sort(function (a, b) {
           return a.localeCompare(b);
         })
-        .join(", ");
+        .join(', ');
       stats[key] = 1;
       colormodel[key] = getColorFromLebenslagenCombination(key, poiColors);
     } else {
-      stats[poi.mainlocationtype.lebenslagen.join(", ")] =
-        stats[poi.mainlocationtype.lebenslagen.join(", ")] + 1;
+      stats[poi.mainlocationtype.lebenslagen.join(', ')] =
+        stats[poi.mainlocationtype.lebenslagen.join(', ')] + 1;
     }
   }
 
@@ -71,7 +92,7 @@ const FilterUI = ({ apps = crossLinkApps }) => {
     <PieChart
       data={piechartData}
       donut={true}
-      title='Verteilung'
+      title="Verteilung"
       legend={false}
       colors={piechartColor}
     />
@@ -101,21 +122,21 @@ const FilterUI = ({ apps = crossLinkApps }) => {
         usedApps.push(app.name);
         additionalAppArray.push(
           <a
-            key={"appLink_" + app.name}
+            key={'appLink_' + app.name}
             style={{
-              textDecoration: "none",
+              textDecoration: 'none',
             }}
             href={app.link}
             target={app.target}
-            rel='noopener noreferrer'
+            rel="noopener noreferrer"
           >
             <Badge
               variant={app.bsStyle}
               style={{
                 backgroundColor: app.backgroundColor,
-                marginRight: "5px",
-                display: "inline-block",
-                color: "white",
+                marginRight: '5px',
+                display: 'inline-block',
+                color: 'white',
               }}
             >
               {app.name}
@@ -131,17 +152,17 @@ const FilterUI = ({ apps = crossLinkApps }) => {
       <div>
         <hr />
         <strong>* Themenspezifische Karten:</strong>
-        {"  "}
+        {'  '}
         <h4
           style={{
             lineHeight: 1.7,
-            wordWrap: "break-word",
-            wordBreak: "normal",
-            lineBreak: "strict",
-            hyphens: "none",
-            overflowWrap: "break-word",
-            WebkitHyphens: "none",
-            MozHyphens: "none",
+            wordWrap: 'break-word',
+            wordBreak: 'normal',
+            lineBreak: 'strict',
+            hyphens: 'none',
+            overflowWrap: 'break-word',
+            WebkitHyphens: 'none',
+            MozHyphens: 'none',
           }}
         >
           {additionalAppArray}
@@ -152,18 +173,18 @@ const FilterUI = ({ apps = crossLinkApps }) => {
 
   return (
     <div>
-      <div align='center'>
+      <div align="center">
         <Button
           style={{
             margin: 4,
             marginLeft: 0,
           }}
-          variant='light'
+          variant="light"
           onClick={() => {
             setAllLebenslagenToFilter(
-              "positiv",
+              'positiv',
               lebenslagen,
-              clearFilter("negativ", filterState, setFilterState),
+              clearFilter('negativ', filterState, setFilterState),
               setFilterState
             );
           }}
@@ -172,23 +193,23 @@ const FilterUI = ({ apps = crossLinkApps }) => {
         </Button>
 
         <Button
-          variant='light'
+          variant="light"
           style={{
             margin: 4,
           }}
           onClick={() => {
-            clearFilter("positiv", filterState, setFilterState);
+            clearFilter('positiv', filterState, setFilterState);
           }}
         >
           keine Themen ausw&auml;hlen
         </Button>
         <Button
-          variant='light'
+          variant="light"
           style={{
             margin: 4,
           }}
           onClick={() => {
-            clearFilter("negativ", filterState, setFilterState);
+            clearFilter('negativ', filterState, setFilterState);
           }}
         >
           keine Themen ausschlie&szlig;en
@@ -198,41 +219,41 @@ const FilterUI = ({ apps = crossLinkApps }) => {
       {widePieChartPlaceholder && (
         <div
           style={{
-            width: "100%",
+            width: '100%',
             // background: "green",
-            display: "flex",
-            flexDirection: "row",
-            flexWrap: "nowrap",
-            justifyContent: "normal",
-            alignItems: "normal",
-            alignContent: "normal",
+            display: 'flex',
+            flexDirection: 'row',
+            flexWrap: 'nowrap',
+            justifyContent: 'normal',
+            alignItems: 'normal',
+            alignContent: 'normal',
           }}
         >
           <div
             style={{
-              display: "block",
+              display: 'block',
               // background: "yellow",
-              flexGrow: "0",
-              flexShrink: "1",
-              flexBasis: "auto",
-              alignSelf: "auto",
-              order: "0",
+              flexGrow: '0',
+              flexShrink: '1',
+              flexBasis: 'auto',
+              alignSelf: 'auto',
+              order: '0',
             }}
           >
             {filterRows}
           </div>
           <div
             style={{
-              display: "block",
+              display: 'block',
 
               // background: "blue",
 
-              flexGrow: "1",
-              flexShrink: "1",
-              flexBasis: "auto",
-              alignSelf: "auto",
-              order: "0",
-              alignContent: "center",
+              flexGrow: '1',
+              flexShrink: '1',
+              flexBasis: 'auto',
+              alignSelf: 'auto',
+              order: '0',
+              alignContent: 'center',
             }}
           >
             {widePieChartPlaceholder}
@@ -242,13 +263,13 @@ const FilterUI = ({ apps = crossLinkApps }) => {
       {narrowPieChartPlaceholder && (
         <div
           style={{
-            display: "block",
+            display: 'block',
             // background: "yellow",
-            flexGrow: "0",
-            flexShrink: "1",
-            flexBasis: "auto",
-            alignSelf: "auto",
-            order: "0",
+            flexGrow: '0',
+            flexShrink: '1',
+            flexBasis: 'auto',
+            alignSelf: 'auto',
+            order: '0',
             paddingLeft: 20,
             paddingRight: 20,
           }}
