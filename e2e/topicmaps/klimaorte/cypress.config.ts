0825f9dfcf67625b7d477a11569733b79d76b640
Pavel <p-olhovoi@bk.ru>
2024-03-13 09:26:10 +0100

    fix saving screenshot of e2e tests

--- e2e/topicmaps/klimaorte/cypress.config.ts
+++ e2e/topicmaps/klimaorte/cypress.config.ts
@@ -2,8 +2,9 @@ import { defineConfig } from 'cypress';
 import { nxE2EPreset } from '@nx/cypress/plugins/cypress-preset';
 
 export default defineConfig({
-  e2e: nxE2EPreset(__dirname, {
+  e2e: nxE2EPreset(__filename, {
     bundler: 'vite',
   }),
   screenshotOnRunFailure: true,
+  screenshotsFolder: './report/screenshots',
 });
