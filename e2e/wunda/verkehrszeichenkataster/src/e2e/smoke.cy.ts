b41e6d158cc15d3b06fc5dfd371c928e1735c052
Pavel <p-olhovoi@bk.ru>
2024-03-13 19:18:29 +0100

    add verkehrszeichenkataster smoke test

--- e2e/wunda/verkehrszeichenkataster/src/e2e/smoke.cy.ts
+++ e2e/wunda/verkehrszeichenkataster/src/e2e/smoke.cy.ts
@@ -1,8 +1,8 @@
-describe('verdis smoke test', () => {
+describe('verkehrszeichenkataster smoke test', () => {
   beforeEach(() => cy.visit('/'));
 
   it('main page show authorization form', () => {
-    cy.contains('VerDIS Desktop').should('exist');
+    cy.contains('Verkehrszeichen Kataster').should('exist');
     cy.get('#username').should('be.visible');
     cy.get('#password').should('be.visible');
     cy.contains('Anmelden').should('be.visible');
