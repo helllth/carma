aacc169886847a4944f550d37f4eb0d68b346d14
Pavel <p-olhovoi@bk.ru>
2024-03-14 17:25:39 +0100

    add auth to lagis smoke test

--- e2e/lagis/lagis-desktop/src/e2e/smoke.cy.ts
+++ e2e/lagis/lagis-desktop/src/e2e/smoke.cy.ts
@@ -1,10 +1,20 @@
 describe('lagis smoke test', () => {
+  let userData;
+
+  before(() => {
+    cy.fixture('devSecrets.json').then((data) => {
+      userData = data;
+    });
+  });
+
   beforeEach(() => cy.visit('/'));
 
   it('main page show authorization form', () => {
     cy.contains('LagIS Desktop').should('exist');
-    cy.get('input[type="email"]').should('be.visible');
-    cy.get('input[type="password"]').should('be.visible');
-    cy.contains('Anmeldung').should('be.visible');
+    // cy.get('input[type="email"]').should('be.visible');
+    cy.get('input[type="email"]').type(userData.cheatingUser);
+    // cy.get('input[type="password"]').should('be.visible');
+    cy.get('input[type="password"]').type(userData.cheatingPassword);
+    cy.get('.ant-btn').click();
   });
 });
