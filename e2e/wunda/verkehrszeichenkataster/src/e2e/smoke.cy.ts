1a48f6e1e8cba2e5692d9d232e985b2fc3348c35
Pavel <p-olhovoi@bk.ru>
2024-03-15 11:52:29 +0100

    add auth to vzk smoke test

--- e2e/wunda/verkehrszeichenkataster/src/e2e/smoke.cy.ts
+++ e2e/wunda/verkehrszeichenkataster/src/e2e/smoke.cy.ts
@@ -1,10 +1,35 @@
 describe('verkehrszeichenkataster smoke test', () => {
-  beforeEach(() => cy.visit('/'));
+  let userData;
 
-  it('main page show authorization form', () => {
+  before(() => {
+    cy.fixture('devSecrets.json').then((data) => {
+      userData = data;
+    });
+  });
+
+  beforeEach(() => {
+    cy.visit('/');
+  });
+
+  it('page show map, dashboard, search, intern and extern buttons', () => {
     cy.contains('Verkehrszeichen Kataster').should('exist');
-    cy.get('#username').should('be.visible');
-    cy.get('#password').should('be.visible');
-    cy.contains('Anmelden').should('be.visible');
+    cy.get('#username').type(userData.cheatingUser);
+    cy.get('#password').type(userData.cheatingPassword);
+    cy.get('.ant-btn').click();
+    cy.get('.ant-input').should('be.visible');
+    cy.contains('Karte').should('be.visible');
+    cy.contains('Anzahl').should('be.visible');
+    cy.contains('Infos').should('be.visible');
+    cy.contains('Andere Infos').should('be.visible');
+    cy.contains('Intern').click();
+    cy.get('ul.flex.items-center.gap-2.list-none.px-0')
+      .children('li')
+      .should('have.length', 1);
+    cy.contains('Extern').click();
+    cy.get('ul.flex.items-center.gap-2.list-none.px-0')
+      .children('li')
+      .should('have.length', 2);
+    cy.get('[aria-label="logout"]').click();
+    cy.contains('Verkehrszeichen Kataster').should('be.visible');
   });
 });
