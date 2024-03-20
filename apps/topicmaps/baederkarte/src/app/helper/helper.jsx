ad76461357f4ec0a0e1bb4ebc5c77937ad66dd0b
David Glogaza <david.glogaza40@googlemail.com>
2024-03-20 16:25:34 +0100

    fix gazetteer data

--- apps/topicmaps/baederkarte/src/app/helper/helper.jsx
+++ apps/topicmaps/baederkarte/src/app/helper/helper.jsx
@@ -9,9 +9,19 @@ export const getGazData = async (setGazData) => {
   const prefix = 'GazData';
   const sources = {};
 
+  sources.adressen = await md5FetchText(prefix, host + '/data/adressen.json');
+  sources.bezirke = await md5FetchText(prefix, host + '/data/bezirke.json');
+  sources.quartiere = await md5FetchText(prefix, host + '/data/quartiere.json');
+  sources.pois = await md5FetchText(prefix, host + '/data/pois.json');
   sources.kitas = await md5FetchText(prefix, host + '/data/kitas.json');
 
-  const gazData = getGazDataForTopicIds(sources, ['kitas']);
+  const gazData = getGazDataForTopicIds(sources, [
+    'pois',
+    'kitas',
+    'bezirke',
+    'quartiere',
+    'adressen',
+  ]);
 
   setGazData(gazData);
 };
