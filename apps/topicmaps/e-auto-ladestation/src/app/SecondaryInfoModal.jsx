404344251edf484f5d04a15147f59098c24ea9ac
David Glogaza <david.glogaza40@googlemail.com>
2024-03-20 13:21:27 +0100

    add footer to info modal

--- apps/topicmaps/e-auto-ladestation/src/app/SecondaryInfoModal.jsx
+++ apps/topicmaps/e-auto-ladestation/src/app/SecondaryInfoModal.jsx
@@ -279,7 +279,30 @@ const SecondaryInfoModal = ({ feature, setOpen }) => {
                 }}
               >
                 <div>
-                  <span style={{ fontSize: '11px' }}></span>
+                  <span style={{ fontSize: '11px' }}>
+                    <div>
+                      <b>TopicMaps Wuppertal</b> (Version 1.22.3):{' '}
+                      <a href="https://cismet.de/" target="_cismet">
+                        cismet GmbH
+                      </a>{' '}
+                      auf Basis von{' '}
+                      <a href="http://leafletjs.com/" target="_more">
+                        Leaflet
+                      </a>{' '}
+                      und{' '}
+                      <a href="https://cismet.de/#refs" target="_cismet">
+                        cids | WuNDa
+                      </a>{' '}
+                      |{' '}
+                      <a
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        href="https://cismet.de/datenschutzerklaerung.html"
+                      >
+                        Datenschutzerklärung (Privacy Policy)
+                      </a>
+                    </div>
+                  </span>
                 </div>
               </td>
               <td>
