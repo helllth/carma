ae77a59c7e113076cc344ae134d2549c4201255c
David Glogaza <david.glogaza40@googlemail.com>
2024-04-02 18:17:39 +0200

    add loading all aevs to redux

--- apps/topicmaps/fnp-test/src/app/App.tsx
+++ apps/topicmaps/fnp-test/src/app/App.tsx
@@ -1,10 +1,19 @@
 import { DocumentViewer } from '@cismet/document-viewer';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'leaflet/dist/leaflet.css';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import 'react-bootstrap-typeahead/css/Typeahead.css';
 import 'react-cismap/topicMaps.css';
+import { loadAEVs } from '../store/slices/aenderungsverfahren';
 
 export function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(loadAEVs());
+  }, []);
+
   return <DocumentViewer />;
 }
 
--- apps/topicmaps/fnp-test/src/store/slices/aenderungsverfahren.ts
+++ apps/topicmaps/fnp-test/src/store/slices/aenderungsverfahren.ts
@@ -7,9 +7,36 @@ const initialState = {
 const slice = createSlice({
   name: 'aev',
   initialState,
-  reducers: {},
+  reducers: {
+    setData(state, action) {
+      state.data = action.payload;
+      return state;
+    },
+  },
 });
 
 export default slice;
 
-export const {} = slice.actions;
+export const loadAEVs = () => {
+  return async (dispatch, getState) => {
+    fetch('https://wunda-geoportal.cismet.de/data/aenderungsv.data.json')
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then((result) => {
+        dispatch(setData(result));
+        console.log(result);
+      })
+      .catch((error) => {
+        console.error(
+          'There was a problem with the fetch operation:',
+          error.message
+        );
+      });
+  };
+};
+
+export const { setData } = slice.actions;
