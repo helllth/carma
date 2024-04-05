010100ac9a79e06de324ead54a4168ac59e411d9
David Glogaza <david.glogaza40@googlemail.com>
2024-04-05 10:29:41 +0200

    styling sidebar

--- libraries/document-viewer/src/lib/components/Sidebar.tsx
+++ libraries/document-viewer/src/lib/components/Sidebar.tsx
@@ -2,20 +2,41 @@ import { useRef } from 'react';
 // @ts-ignore
 import Icon from 'react-cismap/commons/Icon';
 import { Doc } from '../document-viewer';
-import { useLocation, useNavigate, useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFile } from '@fortawesome/free-regular-svg-icons';
+import { ProgressBar } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 interface SidebarProps {
-  docs?: Doc[];
+  docs: Doc[];
   index: number;
+  maxIndex: number;
 }
 
-const Sidebar = ({ docs, index }: SidebarProps) => {
-  const { docPackageId, file, page } = useParams();
+const Sidebar = ({ docs, index, maxIndex }: SidebarProps) => {
+  const { docPackageId, page } = useParams();
   const navigate = useNavigate();
   const sidebarRef = useRef(null);
-  const iconName = 'file-o';
-  const selected = false;
-  const progressBar = undefined;
+
+  const SIDEBAR_FILENAME_SHORTENER = {
+    bplaene: (original: string) => {
+      const ret = original
+        .replace(/.pdf$/, '')
+        .replace(/^BPL_n?a?\d*V?-?(A|B|C)*\d*_(0_)*/, '')
+        .replace(/Info_BPlan-Zusatzdokumente_WUP.*/, 'Info Dateinamen');
+      return ret;
+    },
+    aenderungsv: (original: string) => {
+      return original.replace(/.pdf$/, '').replace(/^FNP_n*\d*_\d*(And)*_/, '');
+    },
+  };
+
+  const filenameShortener = (original: string) => {
+    const shorty = SIDEBAR_FILENAME_SHORTENER['aenderungsv'](original);
+
+    return shorty;
+  };
 
   return (
     <div ref={sidebarRef}>
@@ -24,11 +45,14 @@ const Sidebar = ({ docs, index }: SidebarProps) => {
           docs?.map((doc, i) => (
             <div
               style={{
-                background: `${index - 1 === i ? '#777777' : 'white'}`,
+                background: `${
+                  index - 1 === i ? 'rgb(119, 119, 119)' : '#f5f5f5'
+                }`,
                 height: '100%',
                 padding: 6,
-                marginBottom: 8,
+                marginBottom: '8px',
                 cursor: 'pointer',
+                color: '#333',
               }}
               onClick={() => navigate(`/docs/${docPackageId}/${i + 1}/1`)}
             >
@@ -39,9 +63,15 @@ const Sidebar = ({ docs, index }: SidebarProps) => {
                   alignItems: 'center',
                   display: 'flex',
                   gap: '6px',
+                  padding: 8,
+                  overflowWrap: 'break-word',
                 }}
               >
-                <Icon name="file-pdf-o" size="4x" />
+                {doc.group === 'Zusatzdokumente' ? (
+                  <FontAwesomeIcon icon={faFile} size="3x" />
+                ) : (
+                  <Icon name="file-pdf-o" size="3x" />
+                )}
 
                 <p
                   style={{
@@ -49,10 +79,30 @@ const Sidebar = ({ docs, index }: SidebarProps) => {
                     marginBottom: 5,
                     fontSize: 11,
                     wordWrap: 'break-word',
+                    textWrap: 'pretty',
+                    overflowWrap: 'break-word',
                   }}
                 >
-                  {doc.title || doc.file}
+                  <span>{doc.title || filenameShortener(doc.file)}</span>
                 </p>
+                {index - 1 === i && (
+                  <>
+                    <ProgressBar
+                      style={{
+                        height: '5px',
+                        width: '100%',
+                        marginTop: 0,
+                        marginBottom: 0,
+                      }}
+                      max={maxIndex}
+                      min={0}
+                      now={parseInt(page!)}
+                    />
+                    <p style={{ marginBottom: 0 }}>
+                      {page} / {maxIndex}
+                    </p>
+                  </>
+                )}
               </div>
             </div>
           ))}
