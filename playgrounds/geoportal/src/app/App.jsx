0ef0226af6eef53c860f5a6dd894c6a3c286c42c
Thorsten Hell <thorsten.hell@cismet.de>
2024-04-09 15:58:19 +0200

    wo #2: initial commit

---
+++ playgrounds/geoportal/src/app/App.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import TopicMapContextProvider from 'react-cismap/contexts/TopicMapContextProvider';
+
+import './index.css';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'leaflet/dist/leaflet.css';
+import 'react-bootstrap-typeahead/css/Typeahead.css';
+import 'react-cismap/topicMaps.css';
+import StyledWMSTileLayer from 'react-cismap/StyledWMSTileLayer';
+import TopicMapComponent from 'react-cismap/topicmaps/TopicMapComponent';
+if (typeof global === 'undefined') {
+  window.global = window;
+}
+
+function App() {
+  return (
+    <TopicMapContextProvider>
+      <TopicMapComponent gazData={[]} backgroundlayers="empty">
+        <StyledWMSTileLayer
+          {...{
+            type: 'wmts',
+            url: 'https://geodaten.metropoleruhr.de/spw2/service',
+            layers: 'spw2_light_grundriss',
+            version: '1.3.0',
+            tileSize: 512,
+            transparent: true,
+            opacity: 0.3,
+          }}
+        ></StyledWMSTileLayer>
+      </TopicMapComponent>
+    </TopicMapContextProvider>
+  );
+}
+
+export default App;
