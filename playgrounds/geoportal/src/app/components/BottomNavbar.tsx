14be2243d8ba40f2a8505264a42d3c72963e659e
David Glogaza <david.glogaza40@googlemail.com>
2024-04-12 15:58:00 +0200

    add items to bottom nav

--- playgrounds/geoportal/src/app/components/BottomNavbar.tsx
+++ playgrounds/geoportal/src/app/components/BottomNavbar.tsx
@@ -1,9 +1,16 @@
 import { useSelector } from 'react-redux';
 // @ts-ignore
 import { getMode } from './../store/slices/ui';
-import { Button, Switch } from 'antd';
+import { Button, Radio, Switch } from 'antd';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faShare } from '@fortawesome/free-solid-svg-icons';
+import {
+  faBookmark,
+  faLandmark,
+  faLightbulb,
+  faMap,
+  faShare,
+} from '@fortawesome/free-solid-svg-icons';
+import './switch.css';
 
 const BottomNavbar = () => {
   const mode = useSelector(getMode);
@@ -12,22 +19,28 @@ const BottomNavbar = () => {
     <div
       className={`${
         mode === 'map' && 'hidden'
-      } h-24 w-full flex items-center justify-between p-2 gap-6`}
+      } h-16 w-full flex items-center justify-between py-2 px-[12px] gap-6 relative`}
     >
-      <div className="flex items-center gap-6">
-        <div className="flex items-center">
-          <Button>Hintergrund 1</Button>
-          <Button>Hintergrund 2</Button>
-          <Button>Hintergrund 3</Button>
-        </div>
-        <div className="flex items-center gap-1">
-          <Switch />
-          <span>Blass</span>
-        </div>
-        <div>Legende</div>
-        <div>Messen</div>
+      <p className="mb-0 text-gray-400">Geoportal Wuppertal</p>
+
+      <div className="flex items-center gap-1 absolute left-1/2">
+        <span>Blass</span>
+        <Switch />
       </div>
-      <FontAwesomeIcon icon={faShare} />
+
+      <div className="flex items-center gap-4">
+        <p className="mb-0 text-gray-400">1 : 20 000</p>
+        <Button icon={<FontAwesomeIcon icon={faLandmark} />}>Legende</Button>
+        <Button icon={<FontAwesomeIcon icon={faMap} />}>Modus</Button>
+        <Button icon={<FontAwesomeIcon icon={faBookmark} />}>Teilen</Button>
+        {/* <FontAwesomeIcon icon={faMap} />
+        <FontAwesomeIcon icon={faBookmark} /> */}
+        <Radio.Group value={'standard'}>
+          <Radio.Button value="standard">Standard</Radio.Button>
+          <Radio.Button value="hybrid">Hybrid</Radio.Button>
+          <Radio.Button value="satellit">Satellit</Radio.Button>
+        </Radio.Group>
+      </div>
     </div>
   );
 };
