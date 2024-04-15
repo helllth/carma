90f7c3e358bf36489dea2a06c9c85a4b82701682
David Glogaza <david.glogaza40@googlemail.com>
2024-04-15 09:41:40 +0200

    fix tailwind nesting warning

--- playgrounds/geoportal/postcss.config.js
+++ playgrounds/geoportal/postcss.config.js
@@ -3,6 +3,8 @@ const path = require('path');
 
 module.exports = {
   plugins: {
+    'postcss-import': {},
+    'tailwindcss/nesting': {},
     tailwindcss: {
       config: path.join(__dirname, 'tailwind.config.js'),
     },
