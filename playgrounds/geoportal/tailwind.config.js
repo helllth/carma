61b6f4bac6b41425e0603e98117c72386729cea8
David Glogaza <david.glogaza40@googlemail.com>
2024-04-22 12:26:45 +0200

    fix tailwind config for libraries

--- playgrounds/geoportal/tailwind.config.js
+++ playgrounds/geoportal/tailwind.config.js
@@ -2,7 +2,10 @@
 const path = require('path');
 
 module.exports = {
-  content: [path.join(__dirname, './src/**/*.{js,ts,jsx,tsx}')],
+  content: [
+    path.join(__dirname, './src/**/*.{js,ts,jsx,tsx}'),
+    path.join(__dirname, '../../libraries/layer-lib/src/**/*.{js,ts,jsx,tsx}'),
+  ],
   theme: {
     extend: {},
   },
