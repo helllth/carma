b2ebfab4d0073c175c18d8c285f80d9d3b47f1cc
David Glogaza <david.glogaza40@googlemail.com>
2024-04-22 12:29:29 +0200

    first idea to change layers on click

--- playgrounds/geoportal/src/app/App.tsx
+++ playgrounds/geoportal/src/app/App.tsx
@@ -23,6 +23,27 @@ if (typeof global === 'undefined') {
 
 function App() {
   const [poiColors, setPoiColors] = useState();
+  const [additionalLayers, setAdditionalLayers] = useState<any>();
+
+  const updateLayers = (layer: any) => {
+    const url = layer.getMapUrl.substring(0, layer.getMapUrl.length - 1);
+    const testLayer = {
+      title: layer.title,
+      layer: (
+        <StyledWMSTileLayer
+          key="test"
+          type="wms"
+          url={url}
+          layers={layer.name}
+        />
+      ),
+    };
+    setAdditionalLayers({
+      testLayer: {
+        ...testLayer,
+      },
+    });
+  };
 
   return (
     <TopicMapContextProvider
@@ -30,70 +51,12 @@ function App() {
       referenceSystem={MappingConstants.crs25832}
       referenceSystemDefinition={MappingConstants.proj4crs25832def}
       convertItemToFeature={getConvertItemToFeatureWithPOIColors(poiColors)}
-      additionalLayerConfiguration={{
-        abt9: {
-          title: <span>Abteilung 9</span>,
-          initialActive: false,
-          layer: (
-            <StyledWMSTileLayer
-              key={'abt9'}
-              type="wmts"
-              url="http://s10221.wuppertal-intra.de:8099/abt9_flst/services"
-              layers="abt9"
-              format="image/png"
-              tiled="true"
-              version="1.3.0"
-              tileSize={256}
-              transparent={true}
-              opacity={0.3}
-              pane="additionalLayers1"
-            />
-          ),
-        },
-        baul: {
-          title: <span>Baulastnachweis</span>,
-          initialActive: false,
-          layer: (
-            <StyledWMSTileLayer
-              key={'baul'}
-              type="wmts"
-              url="http://s10221.wuppertal-intra.de:8056/baulasten/services"
-              layers="baul"
-              format="image/png"
-              tiled="true"
-              version="1.1.1"
-              tileSize={256}
-              transparent={true}
-              opacity={0.3}
-              pane="additionalLayers1"
-            />
-          ),
-        },
-        stadt_flurst: {
-          title: <span>Städt. Flurstücke</span>,
-          initialActive: false,
-          layer: (
-            <StyledWMSTileLayer
-              key={'stadt_flurst'}
-              type="wmts"
-              url="http://s10221.wuppertal-intra.de:7098/stadt-flurstuecke/services"
-              layers="stadt_flurst"
-              format="image/png"
-              tiled="true"
-              version="1.1.1"
-              tileSize={256}
-              transparent={true}
-              opacity={0.3}
-              pane="additionalLayers1"
-            />
-          ),
-        },
-      }}
+      additionalLayerConfiguration={additionalLayers}
     >
       <div className="flex flex-col h-screen w-full">
-        <TopNavbar />
+        <TopNavbar setAdditionalLayers={updateLayers} />
 
-        <Map />
+        <Map key={JSON.stringify(additionalLayers)} />
       </div>
     </TopicMapContextProvider>
   );
