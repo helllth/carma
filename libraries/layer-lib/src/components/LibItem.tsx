d6e4a124a61c80df67c8816b4a22f27c44b1c402
David Glogaza <david.glogaza40@googlemail.com>
2024-04-29 16:01:24 +0200

    add favourite icon & buttons on hover

--- libraries/layer-lib/src/components/LibItem.tsx
+++ libraries/layer-lib/src/components/LibItem.tsx
@@ -1,5 +1,17 @@
+import { faStar as regularFaStar } from '@fortawesome/free-regular-svg-icons';
+import {
+  faCircleInfo,
+  faCircleMinus,
+  faCirclePlus,
+  faMinus,
+  faPlus,
+  faStar,
+} from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { Button, Spin } from 'antd';
-import { useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
+// @ts-ignore
+import { TopicMapStylingContext } from 'react-cismap/contexts/TopicMapStylingContextProvider';
 
 interface LayerItemProps {
   setAdditionalLayers: any;
@@ -8,6 +20,10 @@ interface LayerItemProps {
 
 const LibItem = ({ setAdditionalLayers, layer }: LayerItemProps) => {
   const [hovered, setHovered] = useState(false);
+  const [isFavourite, setIsFavourite] = useState(false);
+  const [isActiveLayer, setIsActiveLayer] = useState(false);
+  // @ts-ignore
+  const { additionalLayerConfiguration } = useContext(TopicMapStylingContext);
   const title = layer.Title;
   const description = layer.Abstract;
   const tags = layer.tags.slice(1);
@@ -43,23 +59,26 @@ const LibItem = ({ setAdditionalLayers, layer }: LayerItemProps) => {
 
   const hightlightTextIndexes = undefined;
 
+  useEffect(() => {
+    let setActive = false;
+    for (let keys in additionalLayerConfiguration) {
+      if (
+        keys === name &&
+        additionalLayerConfiguration[keys].url === service.url
+      ) {
+        setActive = true;
+      }
+    }
+    setIsActiveLayer(setActive);
+  }, [additionalLayerConfiguration]);
+
   return (
     <div
       className="flex flex-col rounded-lg w-full shadow-sm h-fit hover:!shadow-lg bg-white"
       onMouseEnter={() => setHovered(true)}
       onMouseLeave={() => setHovered(false)}
     >
-      <button
-        onDoubleClick={(e) => {
-          setAdditionalLayers({
-            name,
-            title,
-            getMapUrl,
-          });
-          console.log(name);
-        }}
-        className="relative overflow-hidden isolate rounded-md flex justify-center items-center w-full aspect-[1.7777/1]"
-      >
+      <div className="relative overflow-hidden isolate rounded-md flex justify-center items-center w-full aspect-[1.7777/1]">
         {isLoading && (
           <div style={{ position: 'absolute', left: '50%' }}>
             <Spin />
@@ -76,19 +95,69 @@ const LibItem = ({ setAdditionalLayers, layer }: LayerItemProps) => {
           }
           alt={title}
           loading="lazy"
-          className="object-cover h-full overflow-clip w-[calc(130%+7.2px)] hover:blur-sm hover:scale-125 transition-all duration-300"
+          className={`object-cover relative h-full overflow-clip w-[calc(130%+7.2px)] ${
+            hovered && 'blur-sm scale-110'
+          } transition-all duration-200`}
           onLoad={(e) => {
             setIsLoading(false);
           }}
         />
-        {/* <div className="absolute inset-0 bg-gray-200 opacity-0 hover:opacity-50 transition duration-150"></div> */}
-        {/* {hovered && (
-          <div className="flex flex-col gap-2 absolute top-0 w-full">
-            <Button>Add</Button>
-            <Button>Delete</Button>
+
+        {isFavourite ? (
+          <FontAwesomeIcon
+            className="absolute right-1 top-1 text-3xl text-yellow-200 cursor-pointer z-50"
+            icon={faStar}
+            onClick={() => setIsFavourite(false)}
+          />
+        ) : (
+          <FontAwesomeIcon
+            className="absolute right-1 top-1 text-3xl cursor-pointer z-50 text-white drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.9)]"
+            icon={regularFaStar}
+            onClick={() => setIsFavourite(true)}
+          />
+          // <StarOutlined className="absolute right-1 top-1 text-3xl text-white drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.9)]" />
+        )}
+        {hovered && (
+          <div className="flex flex-col gap-2 absolute top-0 w-full h-full justify-center p-8 px-10">
+            <button
+              className="w-full bg-gray-100 hover:bg-gray-50 rounded-md py-2 flex text-center items-center justify-center"
+              onClick={() => {
+                setAdditionalLayers({
+                  name,
+                  title,
+                  url: service.url,
+                });
+              }}
+            >
+              {isActiveLayer ? (
+                <>
+                  <FontAwesomeIcon
+                    icon={faCircleMinus}
+                    className="text-lg mr-2"
+                  />{' '}
+                  Entfernen
+                </>
+              ) : (
+                <>
+                  <FontAwesomeIcon
+                    icon={faCirclePlus}
+                    className="text-lg mr-2"
+                  />{' '}
+                  Hinzufügen
+                </>
+              )}
+            </button>
+            <button
+              disabled
+              className="w-full flex text-center items-center justify-center bg-gray-100 hover:bg-gray-50 rounded-md py-2 cursor-not-allowed"
+            >
+              <FontAwesomeIcon icon={faCircleInfo} className="mr-2 text-lg" />
+              {/* <InfoOutlined /> */}
+              Informationen
+            </button>
           </div>
-        )} */}
-      </button>
+        )}
+      </div>
       <div className="flex flex-col gap-2 p-4">
         <h3 className="text-lg">
           {hightlightTextIndexes ? (
