ee24de21898d62d02476cb64cb5127ab4ce2ff5a
David Glogaza <david.glogaza40@googlemail.com>
2024-05-16 09:40:20 +0200

    add gazdata

---
+++ apps/topicmaps/e-bikes/src/helper/gazData.ts
@@ -0,0 +1,25 @@
+import { md5FetchText } from 'react-cismap/tools/fetching';
+import { getGazDataForTopicIds } from 'react-cismap/tools/gazetteerHelper';
+
+import { host } from '../constants/ebikes';
+
+export const getGazData = async (setGazData) => {
+  const prefix = 'GazData';
+  const sources: any = {};
+
+  sources.adressen = await md5FetchText(prefix, host + '/data/adressen.json');
+  sources.bezirke = await md5FetchText(prefix, host + '/data/bezirke.json');
+  sources.quartiere = await md5FetchText(prefix, host + '/data/quartiere.json');
+  sources.pois = await md5FetchText(prefix, host + '/data/pois.json');
+  sources.kitas = await md5FetchText(prefix, host + '/data/kitas.json');
+
+  const gazData = getGazDataForTopicIds(sources, [
+    'pois',
+    'kitas',
+    'bezirke',
+    'quartiere',
+    'adressen',
+  ]);
+
+  setGazData(gazData);
+};
