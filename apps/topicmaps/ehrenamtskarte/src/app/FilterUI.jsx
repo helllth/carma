a05e4ef029efaeab0632ce48d0148f333a9973e2
David Glogaza <david.glogaza40@googlemail.com>
2024-05-16 20:33:24 +0200

    fix filter selection with select

--- apps/topicmaps/ehrenamtskarte/src/app/FilterUI.jsx
+++ apps/topicmaps/ehrenamtskarte/src/app/FilterUI.jsx
@@ -1,40 +1,58 @@
-import { Chart } from 'chart.js';
-import React, { useEffect, useMemo, useState } from 'react';
+import { useMemo } from 'react';
 import { useContext } from 'react';
-import { Badge, Button, Form } from 'react-bootstrap';
 import { Select } from 'antd';
-import ReactChartkick, { PieChart } from 'react-chartkick';
 import {
   FeatureCollectionContext,
   FeatureCollectionDispatchContext,
 } from 'react-cismap/contexts/FeatureCollectionContextProvider';
-import { ResponsiveTopicMapContext } from 'react-cismap/contexts/ResponsiveTopicMapContextProvider';
-
-import { crossLinkApps } from './helper/constants';
-
-import 'url-search-params-polyfill';
-
-ReactChartkick.addAdapter(Chart);
 
 const FilterUI = () => {
+  const { itemsDictionary, filterState } = useContext(FeatureCollectionContext);
+  const { setFilterState } = useContext(FeatureCollectionDispatchContext);
+  const zielgruppen = useMemo(
+    () => itemsDictionary?.zielgruppen || [],
+    [itemsDictionary]
+  );
+
   return (
     <div>
       <h4>Ich suche nach:</h4>
       <Select
         placeholder="Kategorien auswählen ..."
         style={{ width: '100%', marginBottom: 8 }}
-        options={[{ value: 'sample', label: <span>sample</span> }]}
+        options={zielgruppen.map((zielgruppe) => {
+          return {
+            value: zielgruppe,
+            label: zielgruppe,
+          };
+        })}
         mode="multiple"
         allowClear
-        defaultValue={['sample']}
+        value={filterState.positiv}
         onChange={(value) => {
-          console.log(value);
+          const newFilterState = { ...filterState };
+          newFilterState.positiv = value;
+          setFilterState(newFilterState);
         }}
       />
       <h4>Ich schlie&szlig;e aus:</h4>
       <Select
         placeholder="Kategorien auswählen ..."
         style={{ width: '100%' }}
+        options={zielgruppen.map((zielgruppe) => {
+          return {
+            value: zielgruppe,
+            label: zielgruppe,
+          };
+        })}
+        mode="multiple"
+        allowClear
+        value={filterState.negativ}
+        onChange={(value) => {
+          const newFilterState = { ...filterState };
+          newFilterState.negativ = value;
+          setFilterState(newFilterState);
+        }}
       />
     </div>
   );
