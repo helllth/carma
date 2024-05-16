11a9a6140b07bae065bbe89fa734d97d65a648e7
David Glogaza <david.glogaza40@googlemail.com>
2024-05-16 13:49:21 +0200

    fix umfang filter

--- apps/topicmaps/kita-finder/src/app/helper/filter.jsx
+++ apps/topicmaps/kita-finder/src/app/helper/filter.jsx
@@ -4,7 +4,7 @@ const itemFilterFunction = ({ filterState }) => {
 
     if (filterState.inklusion) {
       if (item.plaetze_fuer_behinderte) {
-        result = item.plaetze_fuer_behinderte;
+        result = true;
       }
     }
 
@@ -17,23 +17,38 @@ const itemFilterFunction = ({ filterState }) => {
     if (filterState.alter) {
       if (filterState.alter === 'unter2') {
         if (item.alter === 0) {
-          result = true;
         } else {
           result = false;
         }
       } else if (filterState.alter === 'ab2') {
         if (item.alter === 0 || item.alter === 1) {
-          result = true;
         } else {
           result = false;
         }
       } else if (filterState.alter === 'ab3') {
         if (item.alter < 3) {
-          result = true;
         } else {
           result = false;
         }
       }
+    }
+
+    const chk45h = filterState.umfang_45;
+    const chk35h = filterState.umfang_35;
+
+    if (chk45h && chk35h) {
+    } else if (chk45h) {
+      if (item.stunden === 2 || item.stunden === 1) {
+      } else {
+        result = false;
+      }
+    } else if (chk35h) {
+      if (item.stunden === 0 || item.stunden === 1) {
+      } else {
+        result = false;
+      }
+    } else if (!chk35h && !chk45h) {
+      result = false;
     }
 
     return result;
