426192a850d17507918e77ece66ee76636fcef32
David Glogaza <david.glogaza40@googlemail.com>
2024-05-16 09:41:02 +0200

    add basic map

--- apps/topicmaps/e-bikes/src/app/App.tsx
+++ apps/topicmaps/e-bikes/src/app/App.tsx
@@ -1,37 +1,41 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import styles from './App.module.css';
+import { useEffect } from 'react';
+import { MappingConstants } from 'react-cismap';
+import TopicMapContextProvider from 'react-cismap/contexts/TopicMapContextProvider';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'leaflet/dist/leaflet.css';
+import 'react-bootstrap-typeahead/css/Typeahead.css';
+import 'react-cismap/topicMaps.css';
+import Map from './components/Map';
+import convertItemToFeature from '../helper/convertItemToFeature';
+import {
+  getFeatureStyler,
+  getPoiClusterIconCreatorFunction,
+} from '../helper/styler';
 
 export function App() {
+  useEffect(() => {
+    document.title = 'E-Fahrrad-Karte Wuppertal';
+  }, []);
   return (
-    <div>
-      <h1>
-        <span> Hello there, </span>
-        Welcome e-bikes 👋
-      </h1>
-    </div>
+    <TopicMapContextProvider
+      appKey="EBikeKarteWuppertal2022"
+      featureItemsURL={
+        'https://wupp-topicmaps-data.cismet.de/data/ebikes.data.json'
+      }
+      referenceSystemDefinition={MappingConstants.proj4crs25832def}
+      mapEPSGCode="25832"
+      referenceSystem={MappingConstants.crs25832}
+      getFeatureStyler={getFeatureStyler}
+      featureTooltipFunction={(feature) => feature?.text}
+      convertItemToFeature={convertItemToFeature}
+      clusteringOptions={{
+        iconCreateFunction: getPoiClusterIconCreatorFunction(35),
+      }}
+    >
+      <Map />
+    </TopicMapContextProvider>
   );
 }
 
 export default App;
-
-if (import.meta.vitest) {
-  // add tests related to your file here
-  // For more information please visit the Vitest docs site here: https://vitest.dev/guide/in-source.html
-
-  const { it, expect, beforeEach } = import.meta.vitest;
-  let render: typeof import('@testing-library/react').render;
-
-  beforeEach(async () => {
-    render = (await import('@testing-library/react')).render;
-  });
-
-  it('should render successfully', () => {
-    const { baseElement } = render(<App />);
-    expect(baseElement).toBeTruthy();
-  });
-
-  it('should have a greeting as the title', () => {
-    const { getByText } = render(<App />);
-    expect(getByText(/Welcome e-bikes/gi)).toBeTruthy();
-  });
-}
---
+++ apps/topicmaps/e-bikes/src/app/components/Map.tsx
@@ -0,0 +1,90 @@
+import { useContext, useEffect, useState } from 'react';
+import {
+  FeatureCollectionContext,
+  FeatureCollectionDispatchContext,
+} from 'react-cismap/contexts/FeatureCollectionContextProvider';
+import { TopicMapStylingContext } from 'react-cismap/contexts/TopicMapStylingContextProvider';
+import FeatureCollection from 'react-cismap/FeatureCollection';
+import TopicMapComponent from 'react-cismap/topicmaps/TopicMapComponent';
+import GenericInfoBoxFromFeature from 'react-cismap/topicmaps/GenericInfoBoxFromFeature';
+import { getGazData } from '../../helper/gazData';
+import { getPoiClusterIconCreatorFunction } from '../../helper/styler';
+
+const Map = () => {
+  const [gazData, setGazData] = useState([]);
+  const {
+    setSelectedFeatureByPredicate,
+    setClusteringOptions,
+    setFilterState,
+  } = useContext(FeatureCollectionDispatchContext);
+  const { markerSymbolSize } = useContext(TopicMapStylingContext);
+  const { clusteringOptions } = useContext(FeatureCollectionContext);
+  useEffect(() => {
+    getGazData(setGazData);
+  }, []);
+
+  useEffect(() => {
+    if (markerSymbolSize) {
+      setClusteringOptions({
+        ...clusteringOptions,
+        iconCreateFunction: getPoiClusterIconCreatorFunction,
+      });
+    }
+  }, [markerSymbolSize]);
+
+  useEffect(() => {
+    setFilterState({
+      nur_online: false,
+      oeffnungszeiten: '*',
+      stecker: undefined,
+      nur_gruener_strom: false,
+      nur_schnelllader: false,
+    });
+  }, []);
+
+  return (
+    <TopicMapComponent
+      gazData={gazData}
+      //   modalMenu={<Menu />}
+      locatorControl={true}
+      gazetteerSearchPlaceholder="Ladestation | Stadtteil | Adresse | POI"
+      gazetteerHitTrigger={(hits) => {
+        if ((Array.isArray(hits) && hits[0]?.more?.pid) || hits[0]?.more?.kid) {
+          const gazId = hits[0]?.more?.pid || hits[0]?.more?.kid;
+          setSelectedFeatureByPredicate(
+            (feature) => feature.properties.id === gazId
+          );
+        }
+      }}
+      applicationMenuTooltipString="Filter | Einstellungen | Kompaktanleitung"
+      infoBox={
+        <GenericInfoBoxFromFeature
+          pixelwidth={350}
+          config={{
+            displaySecondaryInfoAction: true,
+            city: 'Wuppertal',
+            navigator: {
+              noun: {
+                singular: 'Ladestation',
+                plural: 'Ladestationen',
+              },
+            },
+            noCurrentFeatureTitle: 'Keine Ladestationen gefunden',
+            noCurrentFeatureContent: (
+              <span>
+                Für mehr Ladestationen Ansicht mit verkleinern oder mit dem
+                untenstehenden Link auf das komplette Stadtgebiet zoomen.
+              </span>
+            ),
+          }}
+          //   photoUrlManipulation={fotoKraemerUrlManipulation}
+          //   captionFactory={fotoKraemerCaptionFactory}
+        />
+      }
+    >
+      <FeatureCollection></FeatureCollection>
+    </TopicMapComponent>
+  );
+};
+
+export default Map;
