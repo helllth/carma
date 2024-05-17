dfd66ba6f5c7b2056ca99025101bfb56e40604c3
Friedrich Hartmann <690920+mxfh@users.noreply.github.com>
2024-05-17 13:18:09 +0200

    add basepath to vite config so cesium-vite-plugin can handle paths

--- playgrounds/cesium/vite.config.ts
+++ playgrounds/cesium/vite.config.ts
@@ -6,6 +6,7 @@ import cesium from 'vite-plugin-cesium';
 
 export default defineConfig({
   root: __dirname,
+  base: './',
   cacheDir: '../../node_modules/.vite/playgrounds/cesium',
 
   server: {
