5849910647c218d286e98a90de79d3830557de02
David Glogaza <david.glogaza40@googlemail.com>
2024-05-22 12:32:48 +0200

    remove console log

--- apps/topicmaps/kulturstadtplan/src/helper/filter.ts
+++ apps/topicmaps/kulturstadtplan/src/helper/filter.ts
@@ -4,8 +4,6 @@ const itemFilterFunction = ({ filterState }) => {
   return (item) => {
     let result = false;
 
-    console.log('xxx', filterState);
-
     if (filterState.mode === 'einrichtungen') {
       const category = classifyMainlocationTypeName(item.mainlocationtype.name);
       if (filterState['einrichtung'].indexOf(category) !== -1) {
