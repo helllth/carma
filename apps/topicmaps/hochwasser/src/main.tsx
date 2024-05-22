b056cbb234479d622de5e4d3c82b7f06e9493c8f
David Glogaza <david.glogaza40@googlemail.com>
2024-05-22 11:34:16 +0200

    add missing global definitions

--- apps/topicmaps/hochwasser/src/main.tsx
+++ apps/topicmaps/hochwasser/src/main.tsx
@@ -11,6 +11,9 @@ import App from './app/App.jsx';
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+if (typeof global === 'undefined') {
+  window.global = window;
+}
 root.render(
   // <StrictMode>
   <App />
