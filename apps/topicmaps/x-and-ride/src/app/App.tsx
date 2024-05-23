362b1d627179cd07a1c7910b1555008271e27f7e
David Glogaza <david.glogaza40@googlemail.com>
2024-05-23 11:48:13 +0200

    add simple map

--- apps/topicmaps/x-and-ride/src/app/App.tsx
+++ apps/topicmaps/x-and-ride/src/app/App.tsx
@@ -1,11 +1,47 @@
+import { useEffect } from 'react';
+import { MappingConstants } from 'react-cismap';
+import TopicMapContextProvider from 'react-cismap/contexts/TopicMapContextProvider';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'leaflet/dist/leaflet.css';
+import 'react-bootstrap-typeahead/css/Typeahead.css';
+import 'react-cismap/topicMaps.css';
+import Map from './components/Map';
+import convertItemToFeature from '../helper/convertItemToFeature';
+import {
+  getFeatureStyler,
+  getPoiClusterIconCreatorFunction,
+} from '../helper/styler';
+
+import titleFactory from '../helper/titleFactory';
+import createItemsDictionary from '../helper/createDictionary';
+import itemFilterFunction from '../helper/filter';
+
 export function App() {
+  useEffect(() => {
+    document.title = 'Park+Ride-Karte Wuppertal';
+  }, []);
   return (
-    <div>
-      <h1>
-        <span> Hello there, </span>
-        Welcome XandRide 👋
-      </h1>
-    </div>
+    <TopicMapContextProvider
+      appKey="XAndRideWuppertal2022"
+      featureItemsURL={
+        'https://wupp-topicmaps-data.cismet.de/data/prbr.data.json'
+      }
+      referenceSystemDefinition={MappingConstants.proj4crs25832def}
+      createFeatureItemsDictionary={createItemsDictionary}
+      mapEPSGCode="25832"
+      referenceSystem={MappingConstants.crs25832}
+      getFeatureStyler={getFeatureStyler}
+      featureTooltipFunction={(feature) => feature?.text}
+      // titleFactory={titleFactory}
+      convertItemToFeature={convertItemToFeature}
+      clusteringOptions={{
+        iconCreateFunction: getPoiClusterIconCreatorFunction(35),
+      }}
+      // itemFilterFunction={itemFilterFunction}
+    >
+      <Map />
+    </TopicMapContextProvider>
   );
 }
 
---
+++ apps/topicmaps/x-and-ride/src/app/components/Map.tsx
@@ -0,0 +1,114 @@
+import { useContext, useEffect, useState } from 'react';
+import {
+  FeatureCollectionContext,
+  FeatureCollectionDispatchContext,
+} from 'react-cismap/contexts/FeatureCollectionContextProvider';
+import { TopicMapStylingContext } from 'react-cismap/contexts/TopicMapStylingContextProvider';
+import FeatureCollection from 'react-cismap/FeatureCollection';
+import TopicMapComponent from 'react-cismap/topicmaps/TopicMapComponent';
+import GenericInfoBoxFromFeature from 'react-cismap/topicmaps/GenericInfoBoxFromFeature';
+import { getGazData } from '../../helper/gazData';
+import {
+  getAllEinrichtungen,
+  getPoiClusterIconCreatorFunction,
+} from '../../helper/styler';
+import Icon from 'react-cismap/commons/Icon';
+import { UIDispatchContext } from 'react-cismap/contexts/UIContextProvider';
+
+const Map = () => {
+  const [gazData, setGazData] = useState([]);
+  // @ts-ignore
+  const { setClusteringOptions, setFilterState } = useContext(
+    FeatureCollectionDispatchContext
+  );
+  // @ts-ignore
+  const { markerSymbolSize } = useContext(TopicMapStylingContext);
+  // @ts-ignore
+  const { clusteringOptions, itemsDictionary } = useContext(
+    FeatureCollectionContext
+  );
+  // @ts-ignore
+  const { setAppMenuActiveMenuSection, setAppMenuVisible } =
+    useContext(UIDispatchContext);
+
+  useEffect(() => {
+    getGazData(setGazData);
+  }, []);
+
+  useEffect(() => {
+    if (markerSymbolSize) {
+      setClusteringOptions({
+        ...clusteringOptions,
+        iconCreateFunction: getPoiClusterIconCreatorFunction,
+      });
+    }
+  }, [markerSymbolSize]);
+
+  useEffect(() => {
+    const einrichtungen = getAllEinrichtungen().map(
+      (einrichtung) => einrichtung
+    );
+    const veranstaltungen = itemsDictionary?.veranstaltungsarten?.map(
+      (veranstaltung) => veranstaltung
+    );
+    setFilterState({
+      einrichtung: einrichtungen,
+      veranstaltung: veranstaltungen,
+      mode: 'einrichtungen',
+    });
+  }, [itemsDictionary]);
+
+  return (
+    <TopicMapComponent
+      gazData={gazData}
+      modalMenu={<></>}
+      locatorControl={true}
+      photoLightBox
+      gazetteerSearchPlaceholder="P+R | B+R | Stadtteil | Adresse | POI"
+      applicationMenuTooltipString="Filter | Einstellungen | Kompaktanleitung"
+      infoBox={
+        <GenericInfoBoxFromFeature
+          pixelwidth={350}
+          config={{
+            city: 'Wuppertal',
+            navigator: {
+              noun: {
+                singular: 'Anlage',
+                plural: 'Anlagen',
+              },
+            },
+            noFeatureTitle: 'Keine Anlagen gefunden!',
+            noCurrentFeatureContent: (
+              <p>
+                Für mehr Anlagen Ansicht mit <Icon name="minus-square" />{' '}
+                verkleinern oder mit untenstehendem Link alle Treffer der
+                aktuellen Filterung anzeigen. Ggf. Filtereinstellungen im
+                <a
+                  onClick={() => {
+                    setAppMenuVisible(true);
+                    setAppMenuActiveMenuSection('filter');
+                  }}
+                  className="renderAsLink"
+                >
+                  {' '}
+                  Anwendungsmenü
+                  <Icon
+                    name="bars"
+                    style={{
+                      color: 'black',
+                    }}
+                  />{' '}
+                </a>
+                zurücksetzen
+              </p>
+            ),
+          }}
+        />
+      }
+    >
+      <FeatureCollection></FeatureCollection>
+    </TopicMapComponent>
+  );
+};
+
+export default Map;
