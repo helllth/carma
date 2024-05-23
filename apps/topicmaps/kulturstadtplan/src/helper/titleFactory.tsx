d8706fd06b497dcf565a5cfba9b63a066b2af3ab
David Glogaza <david.glogaza40@googlemail.com>
2024-05-23 09:41:07 +0200

    add title factory

--- apps/topicmaps/kulturstadtplan/src/helper/titleFactory.tsx
+++ apps/topicmaps/kulturstadtplan/src/helper/titleFactory.tsx
@@ -1,38 +1,86 @@
-const factory = ({ featureCollectionContext }) => {
-  const { filterState } = featureCollectionContext;
-  let filterDescriptions: any = [];
-  if (filterState.stationsart?.includes('Ladestation')) {
-    if (
-      filterState.stationsart.includes('Ladestation') &&
-      !filterState.stationsart.includes('Verleihstation')
-    ) {
-      filterDescriptions.push('nur Ladestationen');
-    } else {
-      filterDescriptions.push('Verleih- und Ladestationen');
-    }
+import { getAllEinrichtungen, textConversion } from './styler';
 
-    if (filterState.nur_online === true) {
-      filterDescriptions.push('verfügbar');
+const factory = ({ featureCollectionContext }) => {
+  const { filterState, itemsDictionary } = featureCollectionContext;
+  let maxFilterCount;
+  let einrichtungsEinschub;
+  let themenstadtplanDesc = '';
+  const filterMode = filterState.mode;
+  const veranstaltungen = itemsDictionary?.veranstaltungsarten?.map(
+    (veranstaltung) => veranstaltung
+  );
+
+  if (filterMode === 'einrichtungen') {
+    maxFilterCount = getAllEinrichtungen().length;
+    einrichtungsEinschub = '';
+
+    if (filterState['einrichtung'] && filterState['einrichtung'].length === 1) {
+      themenstadtplanDesc =
+        'alle ' + einrichtungsEinschub + filterState['einrichtung'][0];
+    } else if (
+      filterState['einrichtung'] &&
+      filterState['einrichtung'].length > 0 &&
+      filterState['einrichtung'].length < 3
+    ) {
+      themenstadtplanDesc =
+        'alle ' +
+        einrichtungsEinschub +
+        filterState['einrichtung'][0] +
+        ' und ' +
+        filterState['einrichtung'][1];
+      if (themenstadtplanDesc.split(' und ').length - 1 > 1) {
+        themenstadtplanDesc = themenstadtplanDesc.replace(' und ', ', ');
+      }
+    } else if (
+      filterState['einrichtung'].length > 0 &&
+      filterState['einrichtung'].length < maxFilterCount
+    ) {
+      themenstadtplanDesc =
+        filterState['einrichtung'].length +
+        ' ' +
+        textConversion('einrichtungen');
     }
-    if (filterState.immer_offen === true) {
-      filterDescriptions.push('24/7');
-    }
-    if (filterState.gruener_strom === true) {
-      filterDescriptions.push('Ökostrom');
-    }
-    if (filterState.ladebox_zu === true) {
-      filterDescriptions.push('Ladebox');
+  } else {
+    einrichtungsEinschub = 'Orte für ';
+    maxFilterCount = veranstaltungen.length;
+    if (
+      filterState['veranstaltung'] &&
+      filterState['veranstaltung'].length === 1
+    ) {
+      themenstadtplanDesc =
+        'alle ' + einrichtungsEinschub + filterState['veranstaltung'][0];
+    } else if (
+      filterState['veranstaltung'] &&
+      filterState['veranstaltung'].length > 0 &&
+      filterState['veranstaltung'].length < 3
+    ) {
+      themenstadtplanDesc =
+        'alle ' +
+        einrichtungsEinschub +
+        filterState['veranstaltung'][0] +
+        ' und ' +
+        filterState['veranstaltung'][1];
+      if (themenstadtplanDesc.split(' und ').length - 1 > 1) {
+        themenstadtplanDesc = themenstadtplanDesc.replace(' und ', ', ');
+      }
+    } else if (
+      filterState['veranstaltung'].length > 0 &&
+      filterState['veranstaltung'].length < maxFilterCount
+    ) {
+      themenstadtplanDesc =
+        filterState['veranstaltung'].length +
+        ' ' +
+        textConversion('veranstaltungen');
     }
   }
-  if (
-    !filterState.stationsart?.includes('Ladestation') &&
-    filterState.stationsart?.includes('Verleihstation')
-  ) {
-    filterDescriptions.push('nur Verleihstationen');
+
+  if (themenstadtplanDesc === '') {
+    return null;
   }
+
   return (
     <div>
-      <b>Meine Stationen: </b> {filterDescriptions.join(' | ')}
+      <b>Mein Kulturstadtplan: </b> {themenstadtplanDesc}
     </div>
   );
 };
