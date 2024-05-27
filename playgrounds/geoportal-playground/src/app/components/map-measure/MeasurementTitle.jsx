87c09dc75dfb5027ebfc72d8df876002b69bda46
PavelOlkhovoi <p-olhovoi@bk.ru>
2024-05-27 20:01:13 +0200

    show old title in new empty

--- playgrounds/geoportal-playground/src/app/components/map-measure/MeasurementTitle.jsx
+++ playgrounds/geoportal-playground/src/app/components/map-measure/MeasurementTitle.jsx
@@ -1,13 +1,33 @@
 import { useState } from 'react';
 const MeasurementTitle = ({ title, shapeId, setMeasurementById }) => {
   const [content, setContent] = useState(title);
+  const [oldContent, setOldContent] = useState(title);
 
   return (
     <div>
       <div
-        onChange={(t) => {
+        // onChange={(t) => {
+        //   console.log('ccc on change', t.currentTarget);
+        //   setContent(t.currentTarget.innerHTML);
+        //   setMeasurementById(shapeId, t.currentTarget.innerHTML);
+        // }}
+        // onInput={(t) => {
+        //   console.log('ccc on change', t.currentTarget.textContent);
+        //   setContent(t.currentTarget.innerHTML);
+        //   setMeasurementById(shapeId, t.currentTarget.innerHTML);
+        // }}
+        onBlur={(t) => {
+          console.log('ccc', t.currentTarget.textContent.length);
           setContent(t.currentTarget.innerHTML);
-          setMeasurementById(shapeId, t.currentTarget.innerHTML);
+          if (t.currentTarget.textContent.length === 0) {
+            console.log('ccc 000', oldContent);
+            setContent(oldContent);
+            t.currentTarget.textContent = oldContent;
+          } else {
+            console.log('ccc else');
+            setContent(t.currentTarget.innerHTML);
+            setMeasurementById(shapeId, t.currentTarget.innerHTML);
+          }
         }}
         onFocus={(t) => {
           console.log('iii focus');
