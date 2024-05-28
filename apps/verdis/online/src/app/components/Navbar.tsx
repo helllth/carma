c9300321fa1b9f116cd5cbfb7ac989eecfa4efd4
David Glogaza <david.glogaza40@googlemail.com>
2024-05-28 13:02:48 +0200

    change navbar styling

--- apps/verdis/online/src/app/components/Navbar.tsx
+++ apps/verdis/online/src/app/components/Navbar.tsx
@@ -30,18 +30,22 @@ import {
   getUiState,
   showChangeRequests,
   showSettings,
+  toggleChartElements,
+  toggleContactElement,
   toggleInfoElements,
 } from '../../store/slices/ui';
 import { colorDraft } from '../../utils/kassenzeichenHelper';
 import Waiting from './Waiting';
 import { useRef } from 'react';
+import './navbar.css';
+import { OverlayTriggerType } from 'react-bootstrap/esm/OverlayTrigger';
+import { logout } from '../../store/slices/auth';
 
 const VerdisOnlineAppNavbar = () => {
   const dispatch = useDispatch();
   const helpRef = useRef(null);
   const kassenzeichen = useSelector(getKassenzeichen);
   const uiState = useSelector(getUiState);
-  console.log('xxx', kassenzeichen);
 
   const crCounter = getNumberOfPendingChanges(kassenzeichen);
   let kasszLabel = 'Kassenzeichen: ';
@@ -58,8 +62,8 @@ const VerdisOnlineAppNavbar = () => {
   let lblExit = 'VerDIS-online beenden';
   let menuIsHidden = false;
 
-  let ttTriggerOn = ['hover', 'focus'];
-  let ttTriggerOff = null;
+  let ttTriggerOn: OverlayTriggerType[] = ['hover', 'focus'];
+  let ttTriggerOff: OverlayTriggerType[] = [];
   let kassenzeichennummer;
   if (kassenzeichen.kassenzeichennummer8) {
     kassenzeichennummer =
@@ -85,6 +89,7 @@ const VerdisOnlineAppNavbar = () => {
           borderRadius: 4,
           backgroundColor: '#222',
           borderColor: '#080808',
+          padding: 0,
         }}
       >
         <Container>
@@ -116,81 +121,75 @@ const VerdisOnlineAppNavbar = () => {
             </>
           </OverlayTrigger>
           <Navbar.Collapse>
-            <Nav className="ml-auto">
-              <NavItem
-                ref={helpRef}
-                id="navitem_showSettings"
-                onClick={() => dispatch(showSettings({ visible: true }))}
-                //   eventKey={2.0}
-                style={{
-                  color: '#9d9d9d',
-                  textShadow: '0 -1px 0 rgba(0, 0, 0, .25)',
-                }}
-              >
-                Hilfe & Einstellungen
-              </NavItem>
-
-              <Overlay
-                //   {...{
-                //     container: this,
-                //     target: this.helpRef,
-                //     show:
-                //       !this.props.uiState.waitingVisible &&
-                //       this.props.uiState.hintVisible,
-                //   }}
-                placement="bottom"
-                target={helpRef}
-              >
-                <Popover
-                  id="popover-basic"
-                  placement="right"
-                  // positionLeft={200}
-                  // positionTop={50}
-                  // _title="Hilfe & Einstellungen"
+            <ul
+              className="nav navbar-right navbar-nav ml-auto"
+              style={{ listStyle: 'none' }}
+            >
+              <li role="presentation">
+                <a
+                  href="#"
+                  role="button"
+                  style={{
+                    color: '#9d9d9d',
+                    backgroundColor: 'transparent',
+                    position: 'relative',
+                    display: 'block',
+                    padding: '10px 15px',
+                    lineHeight: '20px',
+                    paddingTop: '15px',
+                    paddingBottom: '15px',
+                  }}
                 >
-                  Benötigen Sie Unterstützung oder möchten nähere Information,
-                  finden Sie diese hier unter "Hilfe & Einstellungen"
-                </Popover>
-              </Overlay>
-
-              <NavItem
-                id="navitem_showSettings"
-                onClick={() => dispatch(showChangeRequests({ visible: true }))}
-                //   eventKey={2.0}
-                style={{
-                  color: '#9d9d9d',
-                  textShadow: '0 -1px 0 rgba(0, 0, 0, .25)',
-                }}
-              >
-                Änderungswünsche{' '}
-                {uiState.changeRequestsEditMode === true &&
-                  crCounter.crDraftCounter > 0 && (
-                    <Badge style={{ backgroundColor: colorDraft }}>
-                      {crCounter.crDraftCounter}
-                    </Badge>
-                  )}
-                {crCounter.crDraftCounter === 0 && crCounter.crCounter > 0 && (
-                  <Badge>{crCounter.crCounter}</Badge>
-                )}
-              </NavItem>
-              <OverlayTrigger
-                trigger={menuIsHidden ? ttTriggerOff : ttTriggerOn}
-                placement="bottom"
-                overlay={
-                  <Tooltip style={{ zIndex: 3000000000 }} id="prevtt">
-                    {lblDownLoadFeb}
-                  </Tooltip>
-                }
-              >
-                <NavItem
-                  id="navitem_downloadFEB"
-                  // onClick={this.downloadFEB}
-                  // eventKey={2.3}
+                  Hilfe & Einstellungen
+                </a>
+              </li>
+              <li role="presentation">
+                <a
+                  href="#"
+                  role="button"
+                  style={{
+                    color: '#9d9d9d',
+                    backgroundColor: 'transparent',
+                    position: 'relative',
+                    display: 'block',
+                    padding: '10px 15px',
+                    lineHeight: '20px',
+                    paddingTop: '15px',
+                    paddingBottom: '15px',
+                  }}
+                >
+                  Änderungswünsche{' '}
+                  {uiState.changeRequestsEditMode === true &&
+                    crCounter.crDraftCounter > 0 && (
+                      <Badge style={{ backgroundColor: colorDraft }}>
+                        {crCounter.crDraftCounter}
+                      </Badge>
+                    )}
+                  {crCounter.crDraftCounter === 0 &&
+                    crCounter.crCounter > 0 && (
+                      <Badge>{crCounter.crCounter}</Badge>
+                    )}
+                </a>
+              </li>
+              <li role="presentation">
+                <a
+                  href="#"
+                  role="button"
+                  style={{
+                    color: '#9d9d9d',
+                    backgroundColor: 'transparent',
+                    position: 'relative',
+                    display: 'block',
+                    padding: '10px 15px',
+                    lineHeight: '20px',
+                    paddingTop: '15px',
+                    paddingBottom: '15px',
+                  }}
                 >
                   <FontAwesomeIcon icon={faFilePdf} style={pdfIconStyle} />
                   {menuIsHidden ? '   ' + lblDownLoadFeb : ''}
-                </NavItem>
-              </OverlayTrigger>
+                </a>
+              </li>
               <OverlayTrigger
                 trigger={menuIsHidden ? ttTriggerOff : ttTriggerOn}
                 placement="bottom"
@@ -200,16 +199,29 @@ const VerdisOnlineAppNavbar = () => {
                   </Tooltip>
                 }
               >
-                <NavItem
-                  id="navitem_infoElementsEnabled"
+                <li
+                  role="presentation"
                   className={uiState.infoElementsEnabled ? 'active' : ''}
-                  // eventKey={2.1}
-                  // href="#"
-                  onSelect={() => dispatch(toggleInfoElements({}))}
                 >
-                  <FontAwesomeIcon icon={faInfoCircle} />
-                  {menuIsHidden ? '   ' + lblInfo : ''}
-                </NavItem>
+                  <a
+                    // href="#"
+                    role="button"
+                    style={{
+                      color: '#9d9d9d',
+                      backgroundColor: 'transparent',
+                      position: 'relative',
+                      display: 'block',
+                      padding: '10px 15px',
+                      lineHeight: '20px',
+                      paddingTop: '15px',
+                      paddingBottom: '15px',
+                    }}
+                    onClick={() => dispatch(toggleInfoElements({}))}
+                  >
+                    <FontAwesomeIcon icon={faInfoCircle} />
+                    {menuIsHidden ? '   ' + lblInfo : ''}
+                  </a>
+                </li>
               </OverlayTrigger>
               <OverlayTrigger
                 trigger={menuIsHidden ? ttTriggerOff : ttTriggerOn}
@@ -220,16 +232,29 @@ const VerdisOnlineAppNavbar = () => {
                   </Tooltip>
                 }
               >
-                <NavItem
-                  id="navitem_chartElementsEnabled"
+                <li
+                  role="presentation"
                   className={uiState.chartElementsEnabled ? 'active' : ''}
-                  // eventKey={2.2}
-                  // href="#"
-                  // onSelect={this.toggleCharts}
                 >
-                  <FontAwesomeIcon icon={faChartPie} />
-                  {menuIsHidden ? '   ' + lblChart : ''}
-                </NavItem>
+                  <a
+                    // href="#"
+                    role="button"
+                    style={{
+                      color: '#9d9d9d',
+                      backgroundColor: 'transparent',
+                      position: 'relative',
+                      display: 'block',
+                      padding: '10px 15px',
+                      lineHeight: '20px',
+                      paddingTop: '15px',
+                      paddingBottom: '15px',
+                    }}
+                    onClick={() => dispatch(toggleChartElements({}))}
+                  >
+                    <FontAwesomeIcon icon={faChartPie} />
+                    {menuIsHidden ? '   ' + lblChart : ''}
+                  </a>
+                </li>
               </OverlayTrigger>
               <OverlayTrigger
                 trigger={menuIsHidden ? ttTriggerOff : ttTriggerOn}
@@ -240,16 +265,29 @@ const VerdisOnlineAppNavbar = () => {
                   </Tooltip>
                 }
               >
-                <NavItem
-                  id="navitem_contact"
+                <li
+                  role="presentation"
                   className={uiState.contactElementEnabled ? 'active' : ''}
-                  // eventKey={2.6}
-                  // href="#"
-                  // onSelect={this.toggleContact}
                 >
-                  <FontAwesomeIcon icon={faUser} />
-                  {menuIsHidden ? '   ' + lblContact : ''}
-                </NavItem>
+                  <a
+                    // href="#"
+                    role="button"
+                    style={{
+                      color: '#9d9d9d',
+                      backgroundColor: 'transparent',
+                      position: 'relative',
+                      display: 'block',
+                      padding: '10px 15px',
+                      lineHeight: '20px',
+                      paddingTop: '15px',
+                      paddingBottom: '15px',
+                    }}
+                    onClick={() => dispatch(toggleContactElement({}))}
+                  >
+                    <FontAwesomeIcon icon={faUser} />
+                    {menuIsHidden ? '   ' + lblContact : ''}
+                  </a>
+                </li>
               </OverlayTrigger>
               <OverlayTrigger
                 trigger={menuIsHidden ? ttTriggerOff : ttTriggerOn}
@@ -260,15 +298,30 @@ const VerdisOnlineAppNavbar = () => {
                   </Tooltip>
                 }
               >
-                <NavItem
-                  id="navitem_logout"
-                  //   eventKey={3}
-                  //   href="/#/"
-                >
-                  <FontAwesomeIcon icon={faPowerOff} />
-                  {menuIsHidden ? '   ' + lblExit : ''}
-                </NavItem>
+                <li role="presentation">
+                  <a
+                    // href="#"
+                    role="button"
+                    style={{
+                      color: '#9d9d9d',
+                      backgroundColor: 'transparent',
+                      position: 'relative',
+                      display: 'block',
+                      padding: '10px 15px',
+                      lineHeight: '20px',
+                      paddingTop: '15px',
+                      paddingBottom: '15px',
+                    }}
+                    // @ts-ignore
+                    onClick={() => dispatch(logout())}
+                  >
+                    <FontAwesomeIcon icon={faPowerOff} />
+                    {menuIsHidden ? '   ' + lblExit : ''}
+                  </a>
+                </li>
               </OverlayTrigger>
+            </ul>
+            {/* <Nav className="ml-auto">
               {uiState.cloudStorageStatus ===
                 CLOUDSTORAGESTATES.CLOUD_STORAGE_UP && (
                 <NavItem
@@ -309,7 +362,7 @@ const VerdisOnlineAppNavbar = () => {
                   {menuIsHidden ? '   ' + lblExit : ''}
                 </NavItem>
               )}
-            </Nav>
+            </Nav> */}
           </Navbar.Collapse>
         </Container>
       </Navbar>
