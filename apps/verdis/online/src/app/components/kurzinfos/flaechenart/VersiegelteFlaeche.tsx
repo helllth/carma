2b612d6a9667079d438c275f1db8ecae673948b4
David Glogaza <david.glogaza40@googlemail.com>
2024-05-28 13:57:06 +0200

    add info texts

---
+++ apps/verdis/online/src/app/components/kurzinfos/flaechenart/VersiegelteFlaeche.tsx
@@ -0,0 +1,13 @@
+const VersiegelteFlaeche = () => {
+  return (
+    <div>
+      <strong>Versiegelte Fläche:</strong>
+      <br />
+      Vollversiegelte Fläche (Asphalt, Beton, Platten, normale Pflasterung).
+      100% der Fläche sind bei der Entwässerung zu berücksichtigen.
+      <br />
+    </div>
+  );
+};
+
+export default VersiegelteFlaeche;
