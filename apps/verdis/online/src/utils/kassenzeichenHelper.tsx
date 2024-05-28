79a46ba2ad2842f6138dcf086ef8b59e67130055
David Glogaza <david.glogaza40@googlemail.com>
2024-05-28 11:37:45 +0200

    add chart panel

--- apps/verdis/online/src/utils/kassenzeichenHelper.tsx
+++ apps/verdis/online/src/utils/kassenzeichenHelper.tsx
@@ -66,3 +66,300 @@ export const getCRsForFeature = (kassenzeichen, flaechenFeature) => {
     return undefined;
   }
 };
+
+export const getCRsForFlaeche = (kassenzeichen, flaeche) => {
+  if (
+    kassenzeichen.aenderungsanfrage !== undefined &&
+    kassenzeichen.aenderungsanfrage !== null &&
+    kassenzeichen.aenderungsanfrage.flaechen !== undefined &&
+    kassenzeichen.aenderungsanfrage.flaechen[flaeche.flaechenbezeichnung] !==
+      undefined
+  ) {
+    const ret =
+      kassenzeichen.aenderungsanfrage.flaechen[flaeche.flaechenbezeichnung];
+    return ret;
+  } else {
+    return undefined;
+  }
+};
+
+export const hasAttachment = (flaechenCR) => {
+  if (flaechenCR?.nachrichten !== undefined) {
+    for (const nachricht of flaechenCR.nachrichten) {
+      if (nachricht.anhang !== undefined) {
+        return true;
+      }
+    }
+  }
+};
+
+export const kassenzeichenFlaechenSorter = (fa, fb) => {
+  if (!isNaN(fa.flaechenbezeichnung) && !isNaN(fb.flaechenbezeichnung)) {
+    return +fa.flaechenbezeichnung - +fb.flaechenbezeichnung;
+  } else if (!isNaN(fa.flaechenbezeichnung) && isNaN(fb.flaechenbezeichnung)) {
+    return -1;
+  } else if (isNaN(fa.flaechenbezeichnung) && !isNaN(fb.flaechenbezeichnung)) {
+    return 1;
+  } else {
+    if (fa.flaechenbezeichnung < fb.flaechenbezeichnung) {
+      return -1;
+    } else {
+      return 1;
+    }
+  }
+};
+
+export const veranlagungsgrundlage = [
+  {
+    flaechenart: 6,
+    anschlussgrad: 3,
+    veranlagungsschluessel: 0,
+    bezeichner: '999-Rest',
+  },
+  {
+    flaechenart: 1,
+    anschlussgrad: 2,
+    veranlagungsschluessel: 0,
+    bezeichner: '999-Rest',
+  },
+  {
+    flaechenart: 2,
+    anschlussgrad: 2,
+    veranlagungsschluessel: 0,
+    bezeichner: '999-Rest',
+  },
+  {
+    flaechenart: 3,
+    anschlussgrad: 2,
+    veranlagungsschluessel: 0,
+    bezeichner: '999-Rest',
+  },
+  {
+    flaechenart: 4,
+    anschlussgrad: 2,
+    veranlagungsschluessel: 0,
+    bezeichner: '999-Rest',
+  },
+  {
+    flaechenart: 5,
+    anschlussgrad: 2,
+    veranlagungsschluessel: 0,
+    bezeichner: '999-Rest',
+  },
+  {
+    flaechenart: 6,
+    anschlussgrad: 2,
+    veranlagungsschluessel: 0,
+    bezeichner: '999-Rest',
+  },
+  {
+    flaechenart: 1,
+    anschlussgrad: 3,
+    veranlagungsschluessel: 0,
+    bezeichner: '999-Rest',
+  },
+  {
+    flaechenart: 2,
+    anschlussgrad: 3,
+    veranlagungsschluessel: 0,
+    bezeichner: '999-Rest',
+  },
+  {
+    flaechenart: 3,
+    anschlussgrad: 3,
+    veranlagungsschluessel: 0,
+    bezeichner: '999-Rest',
+  },
+  {
+    flaechenart: 4,
+    anschlussgrad: 3,
+    veranlagungsschluessel: 0,
+    bezeichner: '999-Rest',
+  },
+  {
+    flaechenart: 5,
+    anschlussgrad: 3,
+    veranlagungsschluessel: 0,
+    bezeichner: '999-Rest',
+  },
+  // {
+  //     flaechenart: 2,
+  //     anschlussgrad: 4,
+  //     veranlagungsschluessel: 0.25,
+  //     bezeichner: "730-Va-über"
+  // },
+  // {
+  //     flaechenart: 4,
+  //     anschlussgrad: 4,
+  //     veranlagungsschluessel: 0.35,
+  //     bezeichner: "730-Va-über"
+  // },
+  {
+    flaechenart: 6,
+    anschlussgrad: 4,
+    veranlagungsschluessel: 0.35,
+    bezeichner: '740-VFS',
+  },
+  {
+    flaechenart: 2,
+    anschlussgrad: 5,
+    veranlagungsschluessel: 0.5,
+    bezeichner: '715-GDF',
+  },
+  // {
+  //     flaechenart: 1,
+  //     anschlussgrad: 4,
+  //     veranlagungsschluessel: 0.5,
+  //     bezeichner: "730-Va-über"
+  // },
+  // {
+  //     flaechenart: 3,
+  //     anschlussgrad: 4,
+  //     veranlagungsschluessel: 0.5,
+  //     bezeichner: "730-Va-über"
+  // },
+  {
+    flaechenart: 1,
+    anschlussgrad: 4,
+    veranlagungsschluessel: 0.4,
+    bezeichner: '731-Va-über',
+  },
+  {
+    flaechenart: 3,
+    anschlussgrad: 4,
+    veranlagungsschluessel: 0.4,
+    bezeichner: '731-Va-über',
+  },
+  {
+    flaechenart: 4,
+    anschlussgrad: 4,
+    veranlagungsschluessel: 0.4,
+    bezeichner: '731-Va-über',
+  },
+  {
+    flaechenart: 2,
+    anschlussgrad: 1,
+    veranlagungsschluessel: 0.5,
+    bezeichner: '715-GDF',
+  },
+  {
+    flaechenart: 5,
+    anschlussgrad: 4,
+    veranlagungsschluessel: 0.5,
+    bezeichner: '740-VFS',
+  },
+  {
+    flaechenart: 4,
+    anschlussgrad: 5,
+    veranlagungsschluessel: 0.7,
+    bezeichner: '725-LVF',
+  },
+  {
+    flaechenart: 6,
+    anschlussgrad: 1,
+    veranlagungsschluessel: 0.7,
+    bezeichner: '740-VFS',
+  },
+  {
+    flaechenart: 4,
+    anschlussgrad: 1,
+    veranlagungsschluessel: 0.7,
+    bezeichner: '725-LVF',
+  },
+  {
+    flaechenart: 6,
+    anschlussgrad: 5,
+    veranlagungsschluessel: 0.7,
+    bezeichner: '740-VFS',
+  },
+  {
+    flaechenart: 7,
+    anschlussgrad: 5,
+    veranlagungsschluessel: 1,
+    bezeichner: 'VV-750',
+  },
+  {
+    flaechenart: 3,
+    anschlussgrad: 1,
+    veranlagungsschluessel: 1,
+    bezeichner: '720-VF',
+  },
+  {
+    flaechenart: 1,
+    anschlussgrad: 5,
+    veranlagungsschluessel: 1,
+    bezeichner: '710-DF',
+  },
+  {
+    flaechenart: 5,
+    anschlussgrad: 1,
+    veranlagungsschluessel: 1,
+    bezeichner: '740-VFS',
+  },
+  {
+    flaechenart: 3,
+    anschlussgrad: 5,
+    veranlagungsschluessel: 1,
+    bezeichner: '720-VF',
+  },
+  {
+    flaechenart: 5,
+    anschlussgrad: 5,
+    veranlagungsschluessel: 1,
+    bezeichner: '740-VFS',
+  },
+  {
+    flaechenart: 7,
+    anschlussgrad: 1,
+    veranlagungsschluessel: 1,
+    bezeichner: 'VV-750',
+  },
+  {
+    flaechenart: 7,
+    anschlussgrad: 2,
+    veranlagungsschluessel: 1,
+    bezeichner: 'VV-750',
+  },
+  {
+    flaechenart: 7,
+    anschlussgrad: 3,
+    veranlagungsschluessel: 1,
+    bezeichner: 'VV-750',
+  },
+  {
+    flaechenart: 7,
+    anschlussgrad: 4,
+    veranlagungsschluessel: 1,
+    bezeichner: 'VV-750',
+  },
+  {
+    flaechenart: 1,
+    anschlussgrad: 1,
+    veranlagungsschluessel: 1,
+    bezeichner: '710-DF',
+  },
+  {
+    flaechenart: 2,
+    anschlussgrad: 4,
+    veranlagungsschluessel: 0.3,
+    bezeichner: '735-GDF-V',
+  },
+];
+
+export const flaechenartLookupByAbk = {
+  DF: 1,
+  GDF: 2,
+  VF: 3,
+  VFS: 5,
+  LVS: 6,
+  LVF: 4,
+  VV: 7,
+};
+
+export const anschlussgradLookupByAbk = {
+  'angeschl.': 1,
+  'vers.': 2,
+  'direkt OG': 3,
+  'Va-Über': 4,
+  'Bach verrohrt': 5,
+};
