4da0f06cb1c3109898e56ff722cd68bd0a92a5b5
David Glogaza <david.glogaza40@googlemail.com>
2024-05-29 13:24:01 +0200

    add first modal sections

--- apps/verdis/online/src/app/components/helpandsettings/Menu00MainComponent.tsx
+++ apps/verdis/online/src/app/components/helpandsettings/Menu00MainComponent.tsx
@@ -1,7 +1,10 @@
 import ModalApplicationMenu from 'react-cismap/topicmaps/menu/ModalApplicationMenu';
+import Section from 'react-cismap/topicmaps/menu/Section';
 import Introduction from './Menu05Introduction';
 import { getUiState, showSettings } from '../../../store/slices/ui';
 import { useDispatch, useSelector } from 'react-redux';
+import Menu10Datengrundlagen from './Menu10Datengrundlagen';
+import Menu20MeinKassenzeichen from './Menu20MeinKassenzeichen';
 
 const ModalHelpAndInfo = () => {
   const uiState = useSelector(getUiState);
@@ -14,17 +17,22 @@ const ModalHelpAndInfo = () => {
       menuIntroduction={<Introduction />}
       visible={uiState.settingsVisible}
       setVisible={(value) => dispatch(showSettings({ visible: value }))}
-      menuSections={
-        [
-          //   <Section
-          //     key="filter"
-          //     sectionKey="filter"
-          //     sectionTitle={getFilterHeader()}
-          //     sectionBsStyle="primary"
-          //     sectionContent={<FilterUI />}
-          //   />,
-        ]
-      }
+      menuSections={[
+        <Section
+          key="datengrundlage"
+          sectionKey="datengrundlage"
+          sectionTitle="Grundlagen der Datenerhebung"
+          sectionBsStyle="warning"
+          sectionContent={<Menu10Datengrundlagen />}
+        />,
+        <Section
+          key="meinKassenzeichen"
+          sectionKey="meinKassenzeichen"
+          sectionTitle="Mein Kassenzeichen"
+          sectionBsStyle="warning"
+          sectionContent={<Menu20MeinKassenzeichen />}
+        />,
+      ]}
     />
   );
 };
