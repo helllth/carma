d367250664d55b75e97bbe8f0eca1bbc6ef00816
David Glogaza <david.glogaza40@googlemail.com>
2024-06-03 17:03:53 +0200

    add drag & drop to layer buttons

--- playgrounds/geoportal-playground/src/app/components/layers/LayerButton.tsx
+++ playgrounds/geoportal-playground/src/app/components/layers/LayerButton.tsx
@@ -2,6 +2,8 @@ import { faX } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useDispatch } from 'react-redux';
 import { removeLayer } from '../../store/slices/mapping';
+import { useSortable } from '@dnd-kit/sortable';
+import { CSS } from '@dnd-kit/utilities';
 
 interface LayerButtonProps {
   title: string;
@@ -10,14 +12,29 @@ interface LayerButtonProps {
 
 const LayerButton = ({ title, id }: LayerButtonProps) => {
   const dispatch = useDispatch();
+  const { attributes, listeners, setNodeRef, transform, transition } =
+    useSortable({
+      id,
+    });
+
+  const style = { transition, transform: CSS.Transform.toString(transform) };
   return (
-    <div className="w-fit min-w-max flex items-center gap-2 px-3 bg-white rounded-3xl h-8 z-[99999999] shadow-lg">
+    <div
+      ref={setNodeRef}
+      style={style}
+      {...listeners}
+      {...attributes}
+      className="w-fit min-w-max flex items-center gap-2 px-3 bg-white rounded-3xl h-8 z-[99999999] shadow-lg"
+    >
       <span className="text-sm font-medium">{title}</span>
       <FontAwesomeIcon
         icon={faX}
         className="p-1"
         role="button"
-        onClick={() => dispatch(removeLayer(id))}
+        onClick={(e) => {
+          e.preventDefault();
+          dispatch(removeLayer(id));
+        }}
       />
     </div>
   );
--- playgrounds/geoportal-playground/src/app/components/layers/LayerWrapper.tsx
+++ playgrounds/geoportal-playground/src/app/components/layers/LayerWrapper.tsx
@@ -1,19 +1,64 @@
-import { useSelector } from 'react-redux';
-import { getLayers } from '../../store/slices/mapping';
+import { useDispatch, useSelector } from 'react-redux';
+import { getLayers, setLayers } from '../../store/slices/mapping';
 import LayerButton from './LayerButton';
+import {
+  DndContext,
+  PointerSensor,
+  useDroppable,
+  useSensor,
+  useSensors,
+} from '@dnd-kit/core';
+import {
+  SortableContext,
+  horizontalListSortingStrategy,
+  arrayMove,
+} from '@dnd-kit/sortable';
 
 const LayerWrapper = () => {
+  const dispatch = useDispatch();
   const layers = useSelector(getLayers);
+  const { isOver, setNodeRef } = useDroppable({
+    id: 'droppable',
+  });
+  const style = {
+    color: isOver ? 'green' : undefined,
+  };
+
+  const getLayerPos = (id) => layers.findIndex((layer) => layer.id === id);
+
+  const handleDragEnd = (event) => {
+    const { active, over } = event;
+    if (active.id !== over.id) {
+      const originalPos = getLayerPos(active.id);
+      const newPos = getLayerPos(over.id);
+      const newLayers = arrayMove(layers, originalPos, newPos);
+
+      dispatch(setLayers(newLayers));
+    }
+  };
+
+  const sensors = useSensors(
+    useSensor(PointerSensor, { activationConstraint: { distance: 10 } })
+  );
 
   return (
-    <div
-      id="buttonWrapper"
-      className="absolute flex items-center gap-2 w-[calc(100%-60px)] left-20 top-2.5 z-[999]"
-    >
-      {layers.map((layer) => (
-        <LayerButton title={layer.title} id={layer.id} />
-      ))}
-    </div>
+    <DndContext onDragEnd={handleDragEnd} sensors={sensors}>
+      <div
+        ref={setNodeRef}
+        style={style}
+        id="buttonWrapper"
+        className="absolute flex items-center gap-2 w-[calc(100%-60px)] left-20 top-2.5 z-[999]"
+      >
+        <SortableContext
+          items={layers}
+          strategy={horizontalListSortingStrategy}
+        >
+          {layers.map((layer) => (
+            <LayerButton title={layer.title} id={layer.id} />
+          ))}
+        </SortableContext>
+      </div>
+    </DndContext>
   );
 };
 
