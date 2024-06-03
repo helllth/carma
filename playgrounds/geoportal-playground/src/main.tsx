0c828324e629380ec722023a8dd1b8bc81d17cc8
David Glogaza <david.glogaza40@googlemail.com>
2024-06-03 11:36:06 +0200

    ignore warnings

--- playgrounds/geoportal-playground/src/main.tsx
+++ playgrounds/geoportal-playground/src/main.tsx
@@ -22,6 +22,19 @@ const router = createHashRouter([
   },
 ]);
 
+const originalWarn = console.warn.bind(console);
+const originalError = console.error.bind(console);
+console.warn = (message, ...args) => {
+  if (!message.includes('ReactDOM.render is no longer supported in React 18')) {
+    originalWarn(message, ...args);
+  }
+};
+console.error = (message, ...args) => {
+  if (!message.includes('ReactDOM.render is no longer supported in React 18')) {
+    originalError(message, ...args);
+  }
+};
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
