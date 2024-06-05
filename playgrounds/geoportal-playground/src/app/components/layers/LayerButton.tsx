6fd85761ff2e4523812d30001af0f65ad292fc18
David Glogaza <david.glogaza40@googlemail.com>
2024-06-05 10:22:22 +0200

    change settings box position

--- playgrounds/geoportal-playground/src/app/components/layers/LayerButton.tsx
+++ playgrounds/geoportal-playground/src/app/components/layers/LayerButton.tsx
@@ -50,7 +50,7 @@ const LayerButton = ({
 
   const style = { transform: CSS.Translate.toString(transform) };
   return (
-    <div className="relative" ref={buttonRef}>
+    <div>
       <div
         ref={setNodeRef}
         onClick={() =>
@@ -73,56 +73,57 @@ const LayerButton = ({
         />
       </div>
       {showSettings && (
-        <div
-          className={cn(
-            `bg-white absolute top-12 shadow-lg rounded-3xl w-96 flex flex-col gap-2 p-2`,
-            showInfo ? 'h-56' : 'h-10'
-          )}
-          style={{ left: -(168 - buttonRef?.current?.offsetWidth / 2) }}
-        >
-          <div className="flex items-center h-6 gap-6">
-            <FontAwesomeIcon
-              icon={faChevronLeft}
-              className="text-base"
-              role="button"
-              onClick={() => dispatch(setPreviousSelectedLayerIndex())}
-            />
-            <label className="mb-0 text-md font-medium">Transparenz</label>
-            <Slider
-              onFocus={() => {
-                routedMapRef?.leafletMap?.leafletElement.dragging.disable();
-              }}
-              onChange={(value) => {
-                dispatch(changeOpacity({ id, opacity: value }));
-              }}
-              onChangeComplete={() => {
-                routedMapRef?.leafletMap?.leafletElement.dragging.enable();
-              }}
-              value={opacity}
-              min={0}
-              max={1}
-              step={0.1}
-              className="w-full"
-            />
-            <FontAwesomeIcon
-              icon={faInfo}
-              className="text-base"
-              onClick={() => setShowInfo(!showInfo)}
-            />
-            <FontAwesomeIcon
-              icon={faChevronRight}
-              className="text-base"
-              role="button"
-              onClick={() => dispatch(setNextSelectedLayerIndex())}
-            />
-          </div>
+        <div className="absolute top-12 w-[calc(100%-60px)] left-20 pr-72 z-[999] flex justify-center items-center">
+          <div
+            className={cn(
+              `bg-white shadow-lg rounded-3xl w-1/2 flex flex-col gap-2 p-2`,
+              showInfo ? 'h-56' : 'h-10'
+            )}
+          >
+            <div className="flex items-center h-6 gap-6">
+              <FontAwesomeIcon
+                icon={faChevronLeft}
+                className="text-base"
+                role="button"
+                onClick={() => dispatch(setPreviousSelectedLayerIndex())}
+              />
+              <label className="mb-0 text-md font-medium">Transparenz</label>
+              <Slider
+                onFocus={() => {
+                  routedMapRef?.leafletMap?.leafletElement.dragging.disable();
+                }}
+                onChange={(value) => {
+                  dispatch(changeOpacity({ id, opacity: value }));
+                }}
+                onChangeComplete={() => {
+                  routedMapRef?.leafletMap?.leafletElement.dragging.enable();
+                }}
+                value={opacity}
+                min={0}
+                max={1}
+                step={0.1}
+                className="w-full"
+              />
+              <FontAwesomeIcon
+                icon={faInfo}
+                className="text-base"
+                onClick={() => setShowInfo(!showInfo)}
+              />
+              <FontAwesomeIcon
+                icon={faChevronRight}
+                className="text-base"
+                role="button"
+                onClick={() => dispatch(setNextSelectedLayerIndex())}
+              />
+            </div>
 
-          {showInfo && (
-            <>
-              <h3>Informationen</h3>
-              {description && <p>{description}</p>}
-            </>
-          )}
+            {showInfo && (
+              <>
+                <h3>Informationen</h3>
+                {description && <p>{description}</p>}
+              </>
+            )}
+          </div>
         </div>
       )}
     </div>
