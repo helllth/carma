838450cd429212310cf298f12620661f1363da7c
PavelOlkhovoi <p-olhovoi@bk.ru>
2024-06-13 13:11:19 +0200

    add storybook to map control

---
+++ libraries/map-control/src/lib/map-control.stories.tsx
@@ -0,0 +1,24 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { MapControl } from './map-control';
+
+import { within } from '@storybook/testing-library';
+import { expect } from '@storybook/jest';
+
+const meta: Meta<typeof MapControl> = {
+  component: MapControl,
+  title: 'MapControl',
+};
+export default meta;
+type Story = StoryObj<typeof MapControl>;
+
+export const Primary = {
+  args: {},
+};
+
+export const Heading: Story = {
+  args: {},
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    expect(canvas.getByText(/Welcome to MapControl!/gi)).toBeTruthy();
+  },
+};
