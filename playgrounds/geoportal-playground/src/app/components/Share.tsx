45a2d5506c2d7743a5f7cd174fe60afc7ab52c13
David Glogaza <david.glogaza40@googlemail.com>
2024-06-14 14:57:49 +0200

    add checkboxes

--- playgrounds/geoportal-playground/src/app/components/Share.tsx
+++ playgrounds/geoportal-playground/src/app/components/Share.tsx
@@ -1,9 +1,11 @@
-import { Button, message } from 'antd';
+import { Button, Checkbox, Radio, message } from 'antd';
 import { useSelector } from 'react-redux';
 import { getBackgroundLayer, getLayers } from '../store/slices/mapping';
 import LZString from 'lz-string';
 import { useCopyToClipboard } from '@uidotdev/usehooks';
 import { useSearchParams } from 'react-router-dom';
+import { useState } from 'react';
+import './popover.css';
 
 const Share = () => {
   const backgroundLayer = useSelector(getBackgroundLayer);
@@ -11,10 +13,38 @@ const Share = () => {
   const [searchParams, setSearchParams] = useSearchParams();
   const [copiedText, copyToClipboard] = useCopyToClipboard();
   const [messageApi, contextHolder] = message.useMessage();
+  const [mode, setMode] = useState('');
+  const [settings, setSettings] = useState({
+    showLayerButtons: true,
+    layerButtonsDeletable: true,
+  });
   return (
-    <div className="p-2 flex flex-col gap-2">
+    <div className="p-2 flex flex-col gap-3">
       {contextHolder}
       <h4>Teilen</h4>
+      <Radio.Group value={mode} onChange={(e) => setMode(e.target.value)}>
+        <div className="flex items-center gap-1">
+          <h5 className="mb-0 pr-2">Modus:</h5>
+          <Radio value={''}>Geoportal</Radio>
+          <Radio value={'context'}>Publish</Radio>
+        </div>
+      </Radio.Group>
+      <Checkbox
+        value={settings.showLayerButtons}
+        onChange={(e) =>
+          setSettings({ ...settings, showLayerButtons: e.target.checked })
+        }
+      >
+        Layer Buttons anzeigen
+      </Checkbox>
+      <Checkbox
+        value={settings.layerButtonsDeletable}
+        onChange={(e) =>
+          setSettings({ ...settings, layerButtonsDeletable: e.target.checked })
+        }
+      >
+        Layer Buttons entfernbar
+      </Checkbox>
       <Button
         onClick={() => {
           const newConfig = {
