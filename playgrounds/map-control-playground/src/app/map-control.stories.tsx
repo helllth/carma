da0381084739ece534120d43fe38957dac43b999
PavelOlkhovoi <p-olhovoi@bk.ru>
2024-06-16 13:30:53 +0200

    add topicmap context to story

--- playgrounds/map-control-playground/src/app/map-control.stories.tsx
+++ playgrounds/map-control-playground/src/app/map-control.stories.tsx
@@ -1,5 +1,24 @@
 import type { Meta, StoryObj } from '@storybook/react';
+import React, { useEffect } from 'react';
+import { MappingConstants } from 'react-cismap';
+import TopicMapContextProvider from 'react-cismap/contexts/TopicMapContextProvider';
+
+import convertItemToFeature from './helper/convertItemToFeature';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'leaflet/dist/leaflet.css';
+import 'react-bootstrap-typeahead/css/Typeahead.css';
+import 'react-cismap/topicMaps.css';
+import './App.css';
 import Map from './Map';
+import {
+  getFeatureStyler,
+  getPoiClusterIconCreatorFunction,
+} from './helper/styler';
+import './index.css';
+if (typeof global === 'undefined') {
+  window.global = window;
+}
 
 const meta: Meta<typeof Map> = {
   component: Map,
@@ -8,6 +27,28 @@ const meta: Meta<typeof Map> = {
 export default meta;
 type Story = StoryObj<typeof Map>;
 
-export const SimleExample = () => {
-  return <div>Simple story</div>;
+const MapWithProviders = () => {
+  return (
+    <TopicMapContextProvider
+      appKey="OnlineBaederkarteWuppertal2022"
+      featureItemsURL={
+        'https://wupp-topicmaps-data.cismet.de/data/baeder.data.json'
+      }
+      referenceSystemDefinition={MappingConstants.proj4crs25832def}
+      mapEPSGCode="25832"
+      referenceSystem={MappingConstants.crs25832}
+      getFeatureStyler={getFeatureStyler}
+      featureTooltipFunction={(feature) => feature?.text}
+      convertItemToFeature={convertItemToFeature}
+      clusteringOptions={{
+        iconCreateFunction: getPoiClusterIconCreatorFunction({ svgSize: 24 }),
+      }}
+    >
+      <Map />
+    </TopicMapContextProvider>
+  );
+};
+
+export const SimpleExample = () => {
+  return <MapWithProviders />;
 };
