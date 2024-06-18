a37db0d4668dd67b374a92dc19e04232073617e3
David Glogaza <david.glogaza40@googlemail.com>
2024-06-18 18:45:29 +0200

    add more vector layers

--- libraries/layer-lib/src/helper/config.ts
+++ libraries/layer-lib/src/helper/config.ts
@@ -276,13 +276,52 @@ export const vectorConfig: Config = {
       id: 'vecKanal',
       title: 'Kanäle',
       description: 'Vektor Layer der Kanäle',
-      tags: ['Vector Karten'],
+      tags: ['Vector Karten', 'Kanäle'],
       type: 'layer',
       layerType: 'vector',
       thumbnail:
         'https://images.unsplash.com/photo-1618901882511-e7adb73a1ee0?q=80&w=2664&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
       props: {
         style: 'https://omt.map-hosting.de/styles/kanal/style.json',
+      },
+    },
+    {
+      id: 'vecBaeume',
+      title: 'Bäume',
+      description: 'Vektor Layer der Bäume',
+      tags: ['Vector Karten', 'Bäume'],
+      type: 'layer',
+      layerType: 'vector',
+      thumbnail:
+        'https://images.unsplash.com/photo-1618901882511-e7adb73a1ee0?q=80&w=2664&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      props: {
+        style: 'https://omt.map-hosting.de/styles/baeume/style.json',
+      },
+    },
+    {
+      id: 'vecPOIs',
+      title: 'POIs',
+      description: 'Vektor Layer der POIs',
+      tags: ['Vector Karten', 'POIs'],
+      type: 'layer',
+      layerType: 'vector',
+      thumbnail:
+        'https://images.unsplash.com/photo-1618901882511-e7adb73a1ee0?q=80&w=2664&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      props: {
+        style: 'https://omt.map-hosting.de/styles/pois/style.json',
+      },
+    },
+    {
+      id: 'vecSolar',
+      title: 'Solar',
+      description: 'Vektor Layer der Solar Flächen',
+      tags: ['Vector Karten', 'Solar'],
+      type: 'layer',
+      layerType: 'vector',
+      thumbnail:
+        'https://images.unsplash.com/photo-1618901882511-e7adb73a1ee0?q=80&w=2664&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      props: {
+        style: 'https://omt.map-hosting.de/styles/solar/style.json',
       },
     },
   ],
