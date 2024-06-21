f81ed8549ad3695987665708c6747191dd8e3a04
PavelOlkhovoi <p-olhovoi@bk.ru>
2024-06-21 09:02:14 +0200

    add custom sizes in styler

--- libraries/map-control/src/lib/components/ControlButtonStyler.tsx
+++ libraries/map-control/src/lib/components/ControlButtonStyler.tsx
@@ -2,25 +2,30 @@ import React, { ReactNode, useRef, useState, useEffect } from 'react';
 
 interface ControlButtonStylerProps {
   children: ReactNode;
+  width?: string;
+  height?: string;
+  fontSize?: string;
 }
-
-const iconPadding = {
-  backgroundColor: '#fff',
-  border: '2px solid rgba(0, 0, 0, .23)',
-  borderRadius: '4px',
-  width: '35px',
-  height: '35px',
-  textAlign: 'center',
-  cursor: 'pointer',
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
-  fontSize: '18px',
-};
 
 const ControlButtonStyler: React.FC<ControlButtonStylerProps> = ({
   children,
+  width = '35px',
+  height = '35px',
+  fontSize = '18px',
 }) => {
+  const iconPadding = {
+    backgroundColor: '#fff',
+    border: '2px solid rgba(0, 0, 0, .23)',
+    borderRadius: '4px',
+    width,
+    height,
+    textAlign: 'center',
+    cursor: 'pointer',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    fontSize,
+  };
   return <div style={iconPadding}>{children}</div>;
 };
 
