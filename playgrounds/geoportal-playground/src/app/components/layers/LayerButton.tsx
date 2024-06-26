85da9fcaa1c92b5034a1f47c3c7c073e14f1e711
David Glogaza <david.glogaza40@googlemail.com>
2024-06-26 10:48:51 +0200

    show layer number on small screens

--- playgrounds/geoportal-playground/src/app/components/layers/LayerButton.tsx
+++ playgrounds/geoportal-playground/src/app/components/layers/LayerButton.tsx
@@ -54,7 +54,7 @@ const LayerButton = ({
   const showLeftScrollButton = useSelector(getShowLeftScrollButton);
   const showSettings = index === selectedLayerIndex;
   const layersLength = useSelector(getLayers).length;
-  let urlPrefix = window.location.origin + window.location.pathname;
+  const urlPrefix = window.location.origin + window.location.pathname;
   const { attributes, listeners, setNodeRef, transform, transition } =
     useSortable({
       id,
@@ -149,6 +149,7 @@ const LayerButton = ({
             id="icon"
           />
         )}
+        <span className="text-base sm:hidden">{layersLength} Layer</span>
         {!background && (
           <>
             <span className="text-base">{title}</span>
