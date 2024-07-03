639ee99d18a6e25966c1329ca33d277aae211330
David Glogaza <david.glogaza40@googlemail.com>
2024-07-03 13:56:56 +0200

    add user feedback after saving config

--- playgrounds/geoportal-playground/src/app/components/Save.tsx
+++ playgrounds/geoportal-playground/src/app/components/Save.tsx
@@ -1,19 +1,21 @@
 import { faFileExport } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { Button, Input } from 'antd';
+import { Button, Input, message } from 'antd';
 import { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { appendSavedLayerConfig, getLayers } from '../store/slices/mapping';
 import './popover.css';
 
 const Save = () => {
+  const [messageApi, contextHolder] = message.useMessage();
   const [title, setTitle] = useState('');
   const [description, setDescription] = useState('');
   const dispatch = useDispatch();
   const layers = useSelector(getLayers);
 
   return (
-    <div className="p-2 flex flex-col gap-3">
+    <div className="p-2 flex flex-col gap-3 w-80">
+      {contextHolder}
       <div className="flex items-center gap-2">
         <FontAwesomeIcon icon={faFileExport} className="text-xl" />
         <h4 className="mb-0">Speichern</h4>
@@ -46,9 +48,20 @@ const Save = () => {
             type: 'collection',
             layers,
           };
-          dispatch(appendSavedLayerConfig(config));
-          setTitle('');
-          setDescription('');
+          try {
+            dispatch(appendSavedLayerConfig(config));
+            setTitle('');
+            setDescription('');
+            messageApi.open({
+              type: 'success',
+              content: `Konfiguration "${title}" wurde erfolgreich gespeichert.`,
+            });
+          } catch (e) {
+            messageApi.open({
+              type: 'error',
+              content: 'Es gab einen Fehler beim speichern der Konfiguration',
+            });
+          }
         }}
       >
         Konfiguration Speichern
