c5ad979910022b83b713a6c8ebba70df15b2ffea
David Glogaza <david.glogaza40@googlemail.com>
2024-07-09 17:12:21 +0200

    change default background object

--- playgrounds/geoportal-playground/src/app/store/slices/mapping.ts
+++ playgrounds/geoportal-playground/src/app/store/slices/mapping.ts
@@ -38,24 +38,15 @@ const initialState: MappingState = {
   selectedLayerIndex: -2,
   selectedMapLayer: {
     title: 'Stadtplan',
-    id: 'stadtplan',
+    id: 'karte',
     opacity: 1.0,
-    description: `Kartendienst (WMS) des Regionalverbandes Ruhr (RVR). Datengrundlage:
-            <strong>Stadtkarte 2.0</strong>. Wöchentlich in einem automatischen Prozess
-            aktualisierte Zusammenführung des Straßennetzes der OpenStreetMap
-            mit Amtlichen Geobasisdaten des Landes NRW aus den Fachverfahren
-            ALKIS (Gebäude, Flächennutzungen) und ATKIS (Gewässer). © RVR und
-            Kooperationspartner (
-            <a href="https://www.govdata.de/dl-de/by-2-0">
-              Datenlizenz Deutschland - Namensnennung - Version 2.0
-            </a>
-            ). Lizenzen der Ausgangsprodukte:
-            <a href="https://www.govdata.de/dl-de/zero-2-0">
-              Datenlizenz Deutschland - Zero - Version 2.0
-            </a>
-            (Amtliche Geobasisdaten) und
-            <a href="https://opendatacommons.org/licenses/odbl/1-0/">ODbL</a>
-            (OpenStreetMap contributors).`,
+    description: ``,
+    inhalt: `Kartendienst (WMS) des Regionalverbandes Ruhr (RVR). Datengrundlage: Stadtkarte 2.0. Wöchentlich in einem automatischen Prozess aktualisierte Zusammenführung des Straßennetzes der OpenStreetMap mit Amtlichen Geobasisdaten des Landes NRW aus den Fachverfahren ALKIS (Gebäude, Flächennutzungen) und ATKIS (Gewässer). © RVR und Kooperationspartner ( <a href="https://www.govdata.de/dl-de/by-2-0">
+    Datenlizenz Deutschland - Namensnennung - Version 2.0
+  </a>). Lizenzen der Ausgangsprodukte: <a href="https://www.govdata.de/dl-de/zero-2-0">
+    Datenlizenz Deutschland - Zero - Version 2.0
+  </a> (Amtliche Geobasisdaten) und <a href="https://opendatacommons.org/licenses/odbl/1-0/">ODbL</a> (OpenStreetMap contributors).`,
+    eignung: `Der Stadtplan ist der am einfachsten und sichersten interpretierbare Kartenhintergrund, weil er an den von Stadtplänen geprägten Sehgewohnheiten von Kartennutzerinnen und -nutzern anschließt. Durch die schrittweise Reduzierung des Karteninhalts bei kleiner werdenden Maßstäben eignet sich der Stadtplan als Hintergrund für beliebige Maßstäbe. Aktualität: der Gebäudebestand ist durch die wöchentliche Ableitung aus dem Liegenschaftskataster sehr aktuell. Gebäude können sicher identifiziert werden, da bei Detailbetrachtungen alle Hausnummern dargestellt werden.`,
     visible: true,
     layerType: 'wmts',
     props: {
@@ -66,24 +57,15 @@ const initialState: MappingState = {
   },
   backgroundLayer: {
     title: 'Stadtplan',
-    id: 'stadtplan',
+    id: 'karte',
     opacity: 1.0,
-    description: `Kartendienst (WMS) des Regionalverbandes Ruhr (RVR). Datengrundlage:
-            <strong>Stadtkarte 2.0</strong>. Wöchentlich in einem automatischen Prozess
-            aktualisierte Zusammenführung des Straßennetzes der OpenStreetMap
-            mit Amtlichen Geobasisdaten des Landes NRW aus den Fachverfahren
-            ALKIS (Gebäude, Flächennutzungen) und ATKIS (Gewässer). © RVR und
-            Kooperationspartner (
-            <a href="https://www.govdata.de/dl-de/by-2-0">
-              Datenlizenz Deutschland - Namensnennung - Version 2.0
-            </a>
-            ). Lizenzen der Ausgangsprodukte:
-            <a href="https://www.govdata.de/dl-de/zero-2-0">
-              Datenlizenz Deutschland - Zero - Version 2.0
-            </a>
-            (Amtliche Geobasisdaten) und
-            <a href="https://opendatacommons.org/licenses/odbl/1-0/">ODbL</a>
-            (OpenStreetMap contributors).`,
+    description: ``,
+    inhalt: `Kartendienst (WMS) des Regionalverbandes Ruhr (RVR). Datengrundlage: Stadtkarte 2.0. Wöchentlich in einem automatischen Prozess aktualisierte Zusammenführung des Straßennetzes der OpenStreetMap mit Amtlichen Geobasisdaten des Landes NRW aus den Fachverfahren ALKIS (Gebäude, Flächennutzungen) und ATKIS (Gewässer). © RVR und Kooperationspartner ( <a href="https://www.govdata.de/dl-de/by-2-0">
+    Datenlizenz Deutschland - Namensnennung - Version 2.0
+  </a>). Lizenzen der Ausgangsprodukte: <a href="https://www.govdata.de/dl-de/zero-2-0">
+    Datenlizenz Deutschland - Zero - Version 2.0
+  </a> (Amtliche Geobasisdaten) und <a href="https://opendatacommons.org/licenses/odbl/1-0/">ODbL</a> (OpenStreetMap contributors).`,
+    eignung: `Der Stadtplan ist der am einfachsten und sichersten interpretierbare Kartenhintergrund, weil er an den von Stadtplänen geprägten Sehgewohnheiten von Kartennutzerinnen und -nutzern anschließt. Durch die schrittweise Reduzierung des Karteninhalts bei kleiner werdenden Maßstäben eignet sich der Stadtplan als Hintergrund für beliebige Maßstäbe. Aktualität: der Gebäudebestand ist durch die wöchentliche Ableitung aus dem Liegenschaftskataster sehr aktuell. Gebäude können sicher identifiziert werden, da bei Detailbetrachtungen alle Hausnummern dargestellt werden.`,
     visible: true,
     layerType: 'wmts',
     props: {
