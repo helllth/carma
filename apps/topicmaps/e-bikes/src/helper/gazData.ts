2b5a22dfc04c45a0b707ef6ca79e3d39328e1272
David Glogaza <david.glogaza40@googlemail.com>
2024-07-11 12:46:03 +0200

    add ebikes to gaz data

--- apps/topicmaps/e-bikes/src/helper/gazData.ts
+++ apps/topicmaps/e-bikes/src/helper/gazData.ts
@@ -7,6 +7,7 @@ export const getGazData = async (setGazData) => {
   const prefix = 'GazData';
   const sources: any = {};
 
+  sources.ebikes = await md5FetchText(prefix, host + '/data/ebikes.json');
   sources.adressen = await md5FetchText(prefix, host + '/data/adressen.json');
   sources.bezirke = await md5FetchText(prefix, host + '/data/bezirke.json');
   sources.quartiere = await md5FetchText(prefix, host + '/data/quartiere.json');
@@ -14,6 +15,7 @@ export const getGazData = async (setGazData) => {
   sources.kitas = await md5FetchText(prefix, host + '/data/kitas.json');
 
   const gazData = getGazDataForTopicIds(sources, [
+    'ebikes',
     'pois',
     'kitas',
     'bezirke',
