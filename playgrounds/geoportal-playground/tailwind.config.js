e9c19b67dfa085c05ba4e695488a2ba71392c2ae
David Glogaza <david.glogaza40@googlemail.com>
2024-07-16 11:12:20 +0200

    fix tailwind for layer lib

--- playgrounds/geoportal-playground/tailwind.config.js
+++ playgrounds/geoportal-playground/tailwind.config.js
@@ -4,7 +4,10 @@ const path = require('path');
 module.exports = {
   content: [
     path.join(__dirname, './src/**/*.{js,ts,jsx,tsx}'),
-    path.join(__dirname, '../../libraries/layer-lib/src/**/*.{js,ts,jsx,tsx}'),
+    path.join(
+      __dirname,
+      '../../libraries/mapping/carma-map-layers/src/**/*.{js,ts,jsx,tsx}'
+    ),
   ],
   theme: {
     extend: {},
