e0bc519a69e6f86adc8dcf289bda73315e2a7695
David Glogaza <david.glogaza40@googlemail.com>
2024-07-18 17:38:58 +0200

    add tailwindcss

---
+++ playgrounds/feature-info-playground/postcss.config.js
@@ -0,0 +1,13 @@
+/* postcss.config.js */
+const path = require('path');
+
+module.exports = {
+  plugins: {
+    'postcss-import': {},
+    'tailwindcss/nesting': {},
+    tailwindcss: {
+      config: path.join(__dirname, 'tailwind.config.js'),
+    },
+    autoprefixer: {},
+  },
+};
