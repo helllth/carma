54cfdb4ab07038987b61caf002bc3c14a6901ed2
David Glogaza <david.glogaza40@googlemail.com>
2024-07-18 18:37:56 +0200

    add styling

--- playgrounds/feature-info-playground/src/app/App.tsx
+++ playgrounds/feature-info-playground/src/app/App.tsx
@@ -1,9 +1,15 @@
 export function App() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline text-red-400">
-        Hello there!
-      </h1>
+    <div className="flex flex-col items-center h-screen w-full gap-2 p-2">
+      <div className="w-full bg-green-300 rounded-md h-1/3"></div>
+      <div className="flex w-full items-center justify-center gap-2 h-full">
+        <div className="h-full bg-red-300 rounded-md w-2/3"></div>
+        <div className="flex flex-col gap-2 items-center justify-center w-full h-full">
+          <div className="bg-blue-300 rounded-md w-full h-1/3"></div>
+          <div className="bg-yellow-300 rounded-md w-full h-1/3"></div>
+          <div className="bg-purple-300 rounded-md w-full h-1/3"></div>
+        </div>
+      </div>
     </div>
   );
 }
