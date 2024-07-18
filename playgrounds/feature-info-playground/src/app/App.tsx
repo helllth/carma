4f9ef81a8bfbd8207aa091d8c52d5ad08bf57bb4
David Glogaza <david.glogaza40@googlemail.com>
2024-07-18 17:38:58 +0200

    add tailwindcss

--- playgrounds/feature-info-playground/src/app/App.tsx
+++ playgrounds/feature-info-playground/src/app/App.tsx
@@ -1,37 +1,11 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import styles from './App.module.css';
-
 export function App() {
   return (
     <div>
-      <h1>
-        <span> Hello there, </span>
-        Welcome feature-info-playground 👋
+      <h1 className="text-3xl font-bold underline text-red-400">
+        Hello there!
       </h1>
     </div>
   );
 }
 
 export default App;
-
-if (import.meta.vitest) {
-  // add tests related to your file here
-  // For more information please visit the Vitest docs site here: https://vitest.dev/guide/in-source.html
-
-  const { it, expect, beforeEach } = import.meta.vitest;
-  let render: typeof import('@testing-library/react').render;
-
-  beforeEach(async () => {
-    render = (await import('@testing-library/react')).render;
-  });
-
-  it('should render successfully', () => {
-    const { baseElement } = render(<App />);
-    expect(baseElement).toBeTruthy();
-  });
-
-  it('should have a greeting as the title', () => {
-    const { getByText } = render(<App />);
-    expect(getByText(/Welcome feature-info-playground/gi)).toBeTruthy();
-  });
-}
