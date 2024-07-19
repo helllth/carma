d224c453601a66ff0fdcf89ece87c514c7148772
David Glogaza <david.glogaza40@googlemail.com>
2024-07-19 12:51:15 +0200

    remove unused components

--- playgrounds/geoportal-playground/src/app/App.tsx
+++ playgrounds/geoportal-playground/src/app/App.tsx
@@ -2,7 +2,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'leaflet/dist/leaflet.css';
 import 'react-bootstrap-typeahead/css/Typeahead.css';
 import 'react-cismap/topicMaps.css';
-import './App.css';
 import './index.css';
 // @ts-ignore
 import TopicMapContextProvider from 'react-cismap/contexts/TopicMapContextProvider';
