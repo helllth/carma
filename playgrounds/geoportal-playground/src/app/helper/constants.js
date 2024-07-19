d3b10427106bf60aceab66be91bceb6486b0fc8e
David Glogaza <david.glogaza40@googlemail.com>
2024-07-19 13:07:23 +0200

    remove unused constant

--- playgrounds/geoportal-playground/src/app/helper/constants.js
+++ playgrounds/geoportal-playground/src/app/helper/constants.js
@@ -2,28 +2,6 @@ export const host = 'https://wupp-topicmaps-data.cismet.de';
 export const APP_KEY = 'geoportal';
 export const STORAGE_PREFIX = '1';
 
-export let POI_COLORS = {
-  'Freizeit, Sport': '#194761',
-  Mobilität: '#6BB6D7',
-  'Erholung, Religion': '#094409',
-  Gesellschaft: '#B0CBEC',
-  Religion: '#0D0D0D',
-  Gesundheit: '#CB0D0D',
-  'Erholung, Freizeit': '#638555',
-  Sport: '#0141CF',
-  'Freizeit, Kultur': '#B27A08',
-  'Gesellschaft, Kultur': '#E26B0A',
-  'öffentliche Dienstleistungen': '#417DD4',
-  Orientierung: '#BFBFBF',
-  Bildung: '#FFC000',
-  Stadtbild: '#695656',
-  'Gesellschaft, öffentliche Dienstleistungen': '#569AD6',
-  'Dienstleistungen, Freizeit': '#26978F',
-  Dienstleistungen: '#538DD5',
-  'Bildung, Freizeit': '#BBAA1E',
-  Kinderbetreuung: '#00A0B0',
-};
-
 export const namedStyles = {
   default: { opacity: 0.6 },
   night: {
