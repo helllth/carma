67f2dcf64b537fc6212c42bb6f93e4768d6197d5
David Glogaza <david.glogaza40@googlemail.com>
2024-07-22 10:01:06 +0200

    use redux for saving layers

---
+++ playgrounds/feature-info-playground/src/app/helper/constants.ts
@@ -0,0 +1,2 @@
+export const APP_KEY = 'featureInfo';
+export const STORAGE_PREFIX = '1';
---
+++ playgrounds/feature-info-playground/src/app/store/index.ts
@@ -0,0 +1,71 @@
+import { configureStore } from '@reduxjs/toolkit';
+
+import mappingSlice from './slices/mapping';
+
+import { createLogger } from 'redux-logger';
+import { persistReducer } from 'redux-persist';
+import localForage from 'localforage';
+import { APP_KEY, STORAGE_PREFIX } from '../helper/constants';
+
+console.log('store initializing ....');
+
+const customAppKey = new URLSearchParams(window.location.hash).get('appKey');
+
+const devToolsEnabled =
+  new URLSearchParams(window.location.search).get('devToolsEnabled') === 'true';
+console.log('devToolsEnabled:', devToolsEnabled);
+const stateLoggingEnabledFromSearch = new URLSearchParams(
+  window.location.search
+).get('stateLoggingEnabled');
+
+const inProduction = process.env.NODE_ENV === 'production';
+
+console.log('in Production Mode:', inProduction);
+const stateLoggingEnabled =
+  (stateLoggingEnabledFromSearch !== null &&
+    stateLoggingEnabledFromSearch !== 'false') ||
+  !inProduction;
+
+console.log(
+  'stateLoggingEnabled:',
+  stateLoggingEnabledFromSearch,
+  'x',
+  stateLoggingEnabled
+);
+const logger = createLogger({
+  collapsed: true,
+});
+
+let middleware;
+if (stateLoggingEnabled === true) {
+  middleware = (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }).concat(logger);
+} else {
+  middleware = (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    });
+}
+
+const mappingConfig = {
+  key: '@' + APP_KEY + '.' + STORAGE_PREFIX + '.app.mapping',
+  storage: localForage,
+  whitelist: ['layers'],
+};
+
+const store = configureStore({
+  reducer: {
+    mapping: persistReducer(mappingConfig, mappingSlice.reducer),
+  },
+  devTools: devToolsEnabled === true && inProduction === false,
+  middleware,
+});
+export default store;
+
+export type AppStore = typeof store;
+
+export type RootState = ReturnType<AppStore['getState']>;
+
+export type AppDispatch = AppStore['dispatch'];
