f2996712041f8a7229c45748b9ddcdd83414db08
Thorsten Hell <thorsten.hell@cismet.de>
2024-07-23 12:22:02 +0200

    add debug mode for vector

--- playgrounds/vector/src/app/LeafletMap.jsx
+++ playgrounds/vector/src/app/LeafletMap.jsx
@@ -31,15 +31,15 @@ const getGazData = async (setGazData) => {
 
   sources.adressen = await md5FetchText(
     prefix,
-    host + '/data/3857/adressen.json'
+    host + '/data/3857/adressen.json',
   );
   sources.bezirke = await md5FetchText(
     prefix,
-    host + '/data/3857/bezirke.json'
+    host + '/data/3857/bezirke.json',
   );
   sources.quartiere = await md5FetchText(
     prefix,
-    host + '/data/3857/quartiere.json'
+    host + '/data/3857/quartiere.json',
   );
   sources.pois = await md5FetchText(prefix, host + '/data/3857/pois.json');
   sources.kitas = await md5FetchText(prefix, host + '/data/3857/kitas.json');
@@ -205,7 +205,7 @@ const Map = ({ layers, vectorStyles }) => {
                 const selectedFeature = e.hits[0];
                 console.log(
                   'xxxy selectedFeature',
-                  JSON.stringify(selectedFeature, null, 2)
+                  JSON.stringify(selectedFeature, null, 2),
                 );
 
                 const p = selectedFeature.properties;
