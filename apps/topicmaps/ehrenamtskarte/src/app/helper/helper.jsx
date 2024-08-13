e2ece51023227c1a39e50edc568674cf30ae6219
David Glogaza <david.glogaza40@googlemail.com>
2024-08-13 09:44:27 +0200

    add correct gazetteer data

--- apps/topicmaps/ehrenamtskarte/src/app/helper/helper.jsx
+++ apps/topicmaps/ehrenamtskarte/src/app/helper/helper.jsx
@@ -1,21 +1,29 @@
-import { md5FetchJSON, md5FetchText } from 'react-cismap/tools/fetching';
-import { getGazDataForTopicIds } from 'react-cismap/tools/gazetteerHelper';
+import { md5FetchJSON, md5FetchText } from "react-cismap/tools/fetching";
+import { getGazDataForTopicIds } from "react-cismap/tools/gazetteerHelper";
 
-import { host } from './constants';
+import { host } from "./constants";
 
 export const getGazData = async (setGazData) => {
-  const prefix = 'GazData';
+  const prefix = "GazData";
   const sources = {};
 
-  sources.kitas = await md5FetchText(prefix, host + '/data/kitas.json');
+  sources.pois = await md5FetchText(prefix, host + "/data/pois.json");
+  sources.adressen = await md5FetchText(prefix, host + "/data/adressen.json");
+  sources.bezirke = await md5FetchText(prefix, host + "/data/bezirke.json");
+  sources.quartiere = await md5FetchText(prefix, host + "/data/quartiere.json");
 
-  const gazData = getGazDataForTopicIds(sources, ['kitas']);
+  const gazData = getGazDataForTopicIds(sources, [
+    "pois",
+    "bezirke",
+    "quartiere",
+    "adressen",
+  ]);
 
   setGazData(gazData);
 };
 
 export const getPOIColors = async (setPoiColors) => {
-  md5FetchJSON('poi_colors', host + '/data/poi.farben.json').then((data) => {
+  md5FetchJSON("poi_colors", host + "/data/poi.farben.json").then((data) => {
     setPoiColors(data);
   });
 };
