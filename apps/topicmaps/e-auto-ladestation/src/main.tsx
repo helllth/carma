a14860a26d01a1dc8291464ba0d89cfe4f97d1a1
Thorsten Hell <thorsten.hell@cismet.de>
2024-08-16 11:58:28 +0200

    silence warnings (tmp)

--- apps/topicmaps/e-auto-ladestation/src/main.tsx
+++ apps/topicmaps/e-auto-ladestation/src/main.tsx
@@ -1,13 +1,32 @@
-import { StrictMode } from 'react';
-import * as ReactDOM from 'react-dom/client';
+import { StrictMode } from "react";
+import * as ReactDOM from "react-dom/client";
 
-import App from './app/App.jsx';
+import App from "./app/App.jsx";
+
+const originalWarn = console.warn.bind(console);
+const originalError = console.error.bind(console);
+console.warn = (message, ...args) => {
+  if (
+    message &&
+    !message.includes("ReactDOM.render is no longer supported in React 18")
+  ) {
+    originalWarn(message, ...args);
+  }
+};
+console.error = (message, ...args) => {
+  if (
+    message &&
+    !message.includes("ReactDOM.render is no longer supported in React 18")
+  ) {
+    originalError(message, ...args);
+  }
+};
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement,
 );
 root.render(
   <StrictMode>
     <App />
-  </StrictMode>
+  </StrictMode>,
 );
