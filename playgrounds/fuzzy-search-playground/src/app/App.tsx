a191027d1496d34c42285848d48f94dfd0a703b9
PavelOlkhovoi <p-olhovoi@bk.ru>
2024-08-19 18:17:49 +0200

    test fuzzy import

--- playgrounds/fuzzy-search-playground/src/app/App.tsx
+++ playgrounds/fuzzy-search-playground/src/app/App.tsx
@@ -1,47 +1,48 @@
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useRef, useState } from "react";
 
-import ProjSingleGeoJson from 'react-cismap/ProjSingleGeoJson';
-import { getGazDataForTopicIds } from 'react-cismap/tools/gazetteerHelper';
-import SearchComponent from './components/SearchComponent.jsx';
-import TopicMapContextProvider from 'react-cismap/contexts/TopicMapContextProvider';
-import TopicMapComponent from 'react-cismap/topicmaps/TopicMapComponent';
+import ProjSingleGeoJson from "react-cismap/ProjSingleGeoJson";
+import { getGazDataForTopicIds } from "react-cismap/tools/gazetteerHelper";
+import SearchComponent from "./components/SearchComponent.jsx";
+import TopicMapContextProvider from "react-cismap/contexts/TopicMapContextProvider";
+import TopicMapComponent from "react-cismap/topicmaps/TopicMapComponent";
 
-import GazetteerHitDisplay from 'react-cismap/GazetteerHitDisplay';
-import GazetteerSearchComponent from 'react-cismap/GazetteerSearchComponent';
-import GenericInfoBoxFromFeature from 'react-cismap/topicmaps/GenericInfoBoxFromFeature';
-import FeatureCollection from 'react-cismap/FeatureCollection';
-import { MappingConstants } from 'react-cismap';
-import convertItemToFeature from './helper/convertItemToFeature';
-import { md5FetchText, fetchJSON } from 'react-cismap/tools/fetching';
+import GazetteerHitDisplay from "react-cismap/GazetteerHitDisplay";
+import GazetteerSearchComponent from "react-cismap/GazetteerSearchComponent";
+import GenericInfoBoxFromFeature from "react-cismap/topicmaps/GenericInfoBoxFromFeature";
+import FeatureCollection from "react-cismap/FeatureCollection";
+import { MappingConstants } from "react-cismap";
+import convertItemToFeature from "./helper/convertItemToFeature";
+import { md5FetchText, fetchJSON } from "react-cismap/tools/fetching";
+import { LibFuzzySearch } from "@carma-mapping/lib-fuzzy-search";
 
 import {
   getFeatureStyler,
   getPoiClusterIconCreatorFunction,
-} from './helper/styler';
+} from "./helper/styler";
 
-const host = 'https://wupp-topicmaps-data.cismet.de';
+const host = "https://wupp-topicmaps-data.cismet.de";
 
 export const getGazData = async (
   setGazData,
-  topics = ['pois', 'kitas', 'bezirke', 'quartiere', 'adressen']
+  topics = ["pois", "kitas", "bezirke", "quartiere", "adressen"],
 ) => {
-  const prefix = 'GazDataForStories';
+  const prefix = "GazDataForStories";
   const sources: any = {};
 
   sources.adressen = await md5FetchText(
     prefix,
-    host + '/data/3857/adressen.json'
+    host + "/data/3857/adressen.json",
   );
   sources.bezirke = await md5FetchText(
     prefix,
-    host + '/data/3857/bezirke.json'
+    host + "/data/3857/bezirke.json",
   );
   sources.quartiere = await md5FetchText(
     prefix,
-    host + '/data/3857/quartiere.json'
+    host + "/data/3857/quartiere.json",
   );
-  sources.pois = await md5FetchText(prefix, host + '/data/3857/pois.json');
-  sources.kitas = await md5FetchText(prefix, host + '/data/3857/kitas.json');
+  sources.pois = await md5FetchText(prefix, host + "/data/3857/pois.json");
+  sources.kitas = await md5FetchText(prefix, host + "/data/3857/kitas.json");
   // sources.bpklimastandorte = await md5FetchText(
   //   prefix,
   //   host + '/data/3857/bpklimastandorte.json'
@@ -55,7 +56,7 @@ export const getGazData = async (
 export function App() {
   const mapStyle = {
     height: 600,
-    cursor: 'pointer',
+    cursor: "pointer",
   };
   let urlSearchParams = new URLSearchParams(window.location.href);
   const mapRef = useRef(null);
@@ -67,17 +68,18 @@ export function App() {
   }, []);
 
   useEffect(() => {
-    console.log('hit', gazetteerHit);
+    console.log("hit", gazetteerHit);
   }, [gazetteerHit]);
   useEffect(() => {
-    console.log('hit oveyrlay', overlayFeature);
+    console.log("hit oveyrlay", overlayFeature);
   }, [overlayFeature]);
 
   return (
     <TopicMapContextProvider>
       <TopicMapComponent
         gazData={gazData}
-        gazetteerSearchComponent={SearchComponent}
+        // gazetteerSearchComponent={SearchComponent}
+        gazetteerSearchComponent={LibFuzzySearch}
         _gazetteerSearchComponent={GazetteerSearchComponent}
       ></TopicMapComponent>
     </TopicMapContextProvider>
