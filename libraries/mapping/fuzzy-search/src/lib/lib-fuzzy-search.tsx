d6561a68d8feee53cf7d0aeab14b67e2d450e12a
PavelOlkhovoi <p-olhovoi@bk.ru>
2024-08-20 10:41:26 +0200

    add helper function

--- libraries/mapping/fuzzy-search/src/lib/lib-fuzzy-search.tsx
+++ libraries/mapping/fuzzy-search/src/lib/lib-fuzzy-search.tsx
@@ -1,228 +1,25 @@
 import { useEffect, useState, useRef } from "react";
-import type { RefObject } from "react";
 import Fuse from "fuse.js";
 import { AutoComplete, Button, Checkbox } from "antd";
 import { builtInGazetteerHitTrigger } from "react-cismap/tools/gazetteerHelper";
-// import "./fuzzy-search.css";
 import "./fuzzy-search.css";
 import IconComp from "react-cismap/commons/Icon";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-// import { location-dot } from '@fortawesome/free-solid-svg-icons';
 import { faLocationDot } from "@fortawesome/free-solid-svg-icons";
-import L from "leaflet";
 import { BaseSelectRef } from "rc-select";
-
-const renderTitle = (category: string) => {
-  let title = "???";
-  switch (category) {
-    case "pois":
-      title = "POIS";
-      break;
-    case "bpklimastandorte":
-      title = "Klimastandorte";
-      break;
-    case "kitas":
-      title = "Kitas";
-      break;
-    case "bezirke":
-      title = "Bezirke";
-      break;
-    case "quartiere":
-      title = "Quartiere";
-      break;
-    case "adressen":
-      title = "Adressen";
-      break;
-    default:
-      title = category;
-      break;
-  }
-  return <span>{title}</span>;
-};
-
-interface MoreData {
-  zl: number;
-  pid: number;
-}
-
-interface SearchResultItem {
-  sorter: number;
-  string: string;
-  glyph: string;
-  x: number;
-  y: number;
-  more: MoreData;
-  type: string;
-  xSearchData: string;
-}
-
-interface SearchResult<T> {
-  item: T;
-  refIndex: number;
-  score?: number;
-}
-
-interface Option {
-  key: number;
-  label: JSX.Element;
-  value: string;
-  sData: SearchResultItem;
-  options?: Option[];
-}
-
-interface SplittedCategories {
-  [category: string]: any[];
-}
-
-const joinNumberLetter = (name: string) =>
-  name.replace(/(\d+)\s([a-zA-Z])/g, "$1$2");
-
-const renderItem = (address: SearchResultItem) => {
-  const addressLabel = buildAddressWithIconUI(address, false);
-  return {
-    key: address.sorter,
-    value: address.string,
-    label: addressLabel,
-    sData: address,
-  };
-};
-
-function buildAddressWithIconUI(
-  addresObj: SearchResultItem,
-  showScore = false,
-  score?: number,
-) {
-  let icon;
-  if (addresObj.glyph === "pie-chart") {
-    icon = "chart-pie";
-  } else {
-    icon = addresObj.glyph;
-  }
-  const streetLabel = (
-    <div style={{ paddingLeft: "0.3rem" }}>
-      <span style={{ marginRight: "0.4rem" }}>
-        <i className={icon && "fas " + "fa-" + icon}></i>
-        {"  "}
-      </span>
-      <span>
-        {showScore ? (
-          <span>
-            <span>{joinNumberLetter(addresObj.string)}</span>
-            <span style={{ color: "gray" }}> ({score})</span>
-          </span>
-        ) : (
-          joinNumberLetter(addresObj.string)
-        )}
-      </span>
-    </div>
-  );
-
-  return streetLabel;
-}
-
-const generateOptions = (
-  results: SearchResult<SearchResultItem>[],
-  showScore = false,
-) => {
-  return results.map((result, idx) => {
-    const streetLabel = buildAddressWithIconUI(
-      result.item,
-      showScore,
-      result.score,
-    );
-    return {
-      key: result.item.sorter,
-      label: <div>{streetLabel}</div>,
-      value: result.item?.string,
-      sData: result.item,
-    };
-  });
-};
-
-interface GruppedOptions {
-  label?: JSX.Element;
-  options?: Option[];
-}
-
-const mapDataToSearchResult = (data: SearchResult<SearchResultItem>[]) => {
-  const splittedCategories: { [key: string]: Option[] } = {};
-
-  data.forEach((item) => {
-    const address = item.item;
-    const catName = address.type;
-
-    if (splittedCategories.hasOwnProperty(catName)) {
-      splittedCategories[catName].push(renderItem(address));
-    } else {
-      splittedCategories[catName] = [renderItem(address)];
-    }
-  });
-
-  const prepareOptions: GruppedOptions[] = [];
-
-  Object.keys(splittedCategories).forEach((item) => {
-    let optionItem: GruppedOptions = {};
-
-    if (!optionItem.hasOwnProperty(item)) {
-      optionItem.label = renderTitle(item);
-      optionItem.options = splittedCategories[item];
-    }
-
-    prepareOptions.push(optionItem);
-  });
-
-  return prepareOptions;
-};
-
-const preps = [
-  "an",
-  "auf",
-  "hinter",
-  "neben",
-  "über",
-  "unter",
-  "vor",
-  "zwischen",
-  "durch",
-  "für",
-  "gegen",
-  "ohne",
-  "um",
-  "mit",
-  "bei",
-  "nach",
-  "in",
-  "von",
-  "zu",
-  "aus",
-  "bis",
-  "seit",
-  "anstatt",
-  "außerhalb",
-  "innerhalb",
-];
-const articles = ["der", "die", "das", "den", "dem", "des"];
-const stopwords = [...preps, ...articles];
-
-type SearchGazetteerProps = {
-  gazData?: any;
-  setGazetteerHit: (hit: any) => void;
-  gazetteerHit: any;
-  mapRef?: RefObject<{
-    leafletMap?: {
-      leafletElement: L.Map;
-    };
-  }>;
-  // cesiumRef?: Viewer;
-  //overlayFeature: any;
-  setOverlayFeature: (feature: any) => void;
-  //crs?: string;
-  referenceSystem: any;
-  referenceSystemDefinition: any;
-  pixelwidth?: number;
-  ifShowCategories?: boolean;
-  // marker3dStyle?: ModelAsset;
-};
+import {
+  customSort,
+  generateOptions,
+  GruppedOptions,
+  limitSearchResult,
+  mapDataToSearchResult,
+  Option,
+  prepareGazData,
+  removeStopwords,
+  SearchGazetteerProps,
+  SearchResultItem,
+  stopwords,
+} from "../tools/fuzzySearchHelper";
 
 export function LibFuzzySearch({
   gazData,
@@ -445,13 +242,6 @@ export function LibFuzzySearch({
     }
   }, [dropdownContainerRef, options, fireScrollEvent, value]);
 
-  const handleShowCategories = (e) => {
-    setSfStandardSearch(e.target.checked);
-    setOptions([]);
-    setSearchResult([]);
-    setValue("");
-  };
-
   return (
     <div
       style={{
@@ -520,96 +310,8 @@ export function LibFuzzySearch({
           onSelect={(value, option) => handleOnSelect(option)}
           value={value}
           onChange={(value) => setValue(value)}
-        >
-          {/* <Input.Search size="large" placeholder="input here" /> */}
-        </AutoComplete>
+        ></AutoComplete>
       )}
-      {/* <div style={{ marginLeft: '20px', flexShrink: 0 }}>
-    <Checkbox onChange={handleShowCategories} checked={showCategories}>
-      <span style={{ fontSize: '14px' }}>Kategorien anzeigen</span>
-    </Checkbox>
-  </div> */}
     </div>
   );
 }
-
-function removeStopwords(text, stopwords) {
-  const words = text.split(" ");
-  const placeholderWords = words.map((word) => {
-    if (stopwords.includes(word.toLowerCase())) {
-      // Replace each character in the word with an underscore
-      return "_".repeat(word.length);
-    }
-    return word;
-  });
-  return placeholderWords.join(" ");
-}
-
-function prepareGazData(data) {
-  const modifiedData = data.map((item) => {
-    const searchData = item?.string;
-    const stringWithoutStopWords = removeStopwords(searchData, stopwords);
-    const address = {
-      ...item,
-      xSearchData: joinNumberLetter(stringWithoutStopWords),
-    };
-    return address;
-  });
-
-  return modifiedData;
-}
-
-function customSort(a, b) {
-  if (a.score !== b.score) {
-    return a.score - b.score;
-  }
-  if (a.item.type !== b.item.type) {
-    return a.item.type.localeCompare(b.item.type);
-  }
-
-  if (!a.item.sorter || !a.item.sorter) {
-    return a.item.xSearchData.localeCompare(a.item.xSearchData);
-  } else {
-    return a.item.sorter - b.item.sorter;
-  }
-}
-
-function limitSearchResult(searchRes, limit, cut = 0.4) {
-  console.log("xxx", searchRes[0].score);
-  let limitedScore = searchRes[0].score < cut ? searchRes[0].score : cut;
-  let countOfCategories = 1;
-  searchRes.forEach((r) => {
-    if (r.score <= cut && r.score > limitedScore && countOfCategories < limit) {
-      limitedScore = r.score;
-      countOfCategories += 1;
-    }
-  });
-
-  const limitedresults = searchRes.filter((r) => r.score <= limitedScore);
-
-  return limitedresults;
-}
-
-function customSortDigit(a, b) {
-  const newA = a.item.xSearchData;
-  const newB = b.item.xSearchData;
-
-  const numA = newA.match(/\d+/) ? parseInt(newA.match(/\d+/)[0]) : null;
-  const numB = newB.match(/\d+/) ? parseInt(newB.match(/\d+/)[0]) : null;
-
-  if (numA !== null && numB !== null) {
-    if (numA !== numB) {
-      return numA - numB;
-    } else {
-      return newA.localeCompare(newB);
-    }
-  } else if (numA !== null) {
-    return 1;
-  } else if (numB !== null) {
-    return -1;
-  } else {
-    return newA.localeCompare(newB);
-  }
-}
-
-export default LibFuzzySearch;
---
+++ libraries/mapping/fuzzy-search/src/tools/fuzzySearchHelper.tsx
@@ -0,0 +1,253 @@
+import L from "leaflet";
+import type { RefObject } from "react";
+
+export type SearchGazetteerProps = {
+  gazData?: any;
+  setGazetteerHit: (hit: any) => void;
+  gazetteerHit: any;
+  mapRef?: RefObject<{
+    leafletMap?: {
+      leafletElement: L.Map;
+    };
+  }>;
+  // cesiumRef?: Viewer;
+  //overlayFeature: any;
+  setOverlayFeature: (feature: any) => void;
+  //crs?: string;
+  referenceSystem: any;
+  referenceSystemDefinition: any;
+  pixelwidth?: number;
+  ifShowCategories?: boolean;
+  // marker3dStyle?: ModelAsset;
+};
+interface MoreData {
+  zl: number;
+  pid: number;
+}
+export interface SearchResultItem {
+  sorter: number;
+  string: string;
+  glyph: string;
+  x: number;
+  y: number;
+  more: MoreData;
+  type: string;
+  xSearchData: string;
+}
+export interface SearchResult<T> {
+  item: T;
+  refIndex: number;
+  score?: number;
+}
+export interface Option {
+  key: number;
+  label: JSX.Element;
+  value: string;
+  sData: SearchResultItem;
+  options?: Option[];
+}
+export interface GruppedOptions {
+  label?: JSX.Element;
+  options?: Option[];
+}
+export const renderTitle = (category: string) => {
+  let title = "???";
+  switch (category) {
+    case "pois":
+      title = "POIS";
+      break;
+    case "bpklimastandorte":
+      title = "Klimastandorte";
+      break;
+    case "kitas":
+      title = "Kitas";
+      break;
+    case "bezirke":
+      title = "Bezirke";
+      break;
+    case "quartiere":
+      title = "Quartiere";
+      break;
+    case "adressen":
+      title = "Adressen";
+      break;
+    default:
+      title = category;
+      break;
+  }
+  return <span>{title}</span>;
+};
+export const joinNumberLetter = (name: string) =>
+  name.replace(/(\d+)\s([a-zA-Z])/g, "$1$2");
+export const renderItem = (address: SearchResultItem) => {
+  const addressLabel = buildAddressWithIconUI(address, false);
+  return {
+    key: address.sorter,
+    value: address.string,
+    label: addressLabel,
+    sData: address,
+  };
+};
+export function buildAddressWithIconUI(
+  addresObj: SearchResultItem,
+  showScore = false,
+  score?: number,
+) {
+  let icon;
+  if (addresObj.glyph === "pie-chart") {
+    icon = "chart-pie";
+  } else {
+    icon = addresObj.glyph;
+  }
+  const streetLabel = (
+    <div style={{ paddingLeft: "0.3rem" }}>
+      <span style={{ marginRight: "0.4rem" }}>
+        <i className={icon && "fas " + "fa-" + icon}></i>
+        {"  "}
+      </span>
+      <span>
+        {showScore ? (
+          <span>
+            <span>{joinNumberLetter(addresObj.string)}</span>
+            <span style={{ color: "gray" }}> ({score})</span>
+          </span>
+        ) : (
+          joinNumberLetter(addresObj.string)
+        )}
+      </span>
+    </div>
+  );
+
+  return streetLabel;
+}
+export const generateOptions = (
+  results: SearchResult<SearchResultItem>[],
+  showScore = false,
+) => {
+  return results.map((result, idx) => {
+    const streetLabel = buildAddressWithIconUI(
+      result.item,
+      showScore,
+      result.score,
+    );
+    return {
+      key: result.item.sorter,
+      label: <div>{streetLabel}</div>,
+      value: result.item?.string,
+      sData: result.item,
+    };
+  });
+};
+export const mapDataToSearchResult = (
+  data: SearchResult<SearchResultItem>[],
+) => {
+  const splittedCategories: { [key: string]: Option[] } = {};
+
+  data.forEach((item) => {
+    const address = item.item;
+    const catName = address.type;
+
+    if (splittedCategories.hasOwnProperty(catName)) {
+      splittedCategories[catName].push(renderItem(address));
+    } else {
+      splittedCategories[catName] = [renderItem(address)];
+    }
+  });
+
+  const prepareOptions: GruppedOptions[] = [];
+
+  Object.keys(splittedCategories).forEach((item) => {
+    let optionItem: GruppedOptions = {};
+
+    if (!optionItem.hasOwnProperty(item)) {
+      optionItem.label = renderTitle(item);
+      optionItem.options = splittedCategories[item];
+    }
+
+    prepareOptions.push(optionItem);
+  });
+
+  return prepareOptions;
+};
+const preps = [
+  "an",
+  "auf",
+  "hinter",
+  "neben",
+  "über",
+  "unter",
+  "vor",
+  "zwischen",
+  "durch",
+  "für",
+  "gegen",
+  "ohne",
+  "um",
+  "mit",
+  "bei",
+  "nach",
+  "in",
+  "von",
+  "zu",
+  "aus",
+  "bis",
+  "seit",
+  "anstatt",
+  "außerhalb",
+  "innerhalb",
+];
+const articles = ["der", "die", "das", "den", "dem", "des"];
+export const stopwords = [...preps, ...articles];
+export function removeStopwords(text, stopwords) {
+  const words = text.split(" ");
+  const placeholderWords = words.map((word) => {
+    if (stopwords.includes(word.toLowerCase())) {
+      // Replace each character in the word with an underscore
+      return "_".repeat(word.length);
+    }
+    return word;
+  });
+  return placeholderWords.join(" ");
+}
+export function prepareGazData(data) {
+  const modifiedData = data.map((item) => {
+    const searchData = item?.string;
+    const stringWithoutStopWords = removeStopwords(searchData, stopwords);
+    const address = {
+      ...item,
+      xSearchData: joinNumberLetter(stringWithoutStopWords),
+    };
+    return address;
+  });
+
+  return modifiedData;
+}
+export function customSort(a, b) {
+  if (a.score !== b.score) {
+    return a.score - b.score;
+  }
+  if (a.item.type !== b.item.type) {
+    return a.item.type.localeCompare(b.item.type);
+  }
+
+  if (!a.item.sorter || !a.item.sorter) {
+    return a.item.xSearchData.localeCompare(a.item.xSearchData);
+  } else {
+    return a.item.sorter - b.item.sorter;
+  }
+}
+export function limitSearchResult(searchRes, limit, cut = 0.4) {
+  console.log("xxx", searchRes[0].score);
+  let limitedScore = searchRes[0].score < cut ? searchRes[0].score : cut;
+  let countOfCategories = 1;
+  searchRes.forEach((r) => {
+    if (r.score <= cut && r.score > limitedScore && countOfCategories < limit) {
+      limitedScore = r.score;
+      countOfCategories += 1;
+    }
+  });
+
+  const limitedresults = searchRes.filter((r) => r.score <= limitedScore);
+
+  return limitedresults;
+}
