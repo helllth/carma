112ac665a9549b0a37902a655fc32d4f44386b7e
PavelOlkhovoi <p-olhovoi@bk.ru>
2024-08-21 08:32:58 +0200

    change export in cesium

--- playgrounds/cesium/src/app/config/app.config.ts
+++ playgrounds/cesium/src/app/config/app.config.ts
@@ -1,5 +1,5 @@
-export const APP_BASE_PATH = process.env.BASE_URL || '/';
-// export const APP_BASE_PATH = import.meta.env.BASE_URL; // TODO USE Bunder with es2022 agian later
+//export const APP_BASE_PATH = process.env.BASE_URL || '/';
+export const APP_BASE_PATH = import.meta.env.BASE_URL; // TODO USE Bunder with es2022 agian later
 
-const CESIUM_PATHNAME = '__cesium__';
+const CESIUM_PATHNAME = "__cesium__";
 export const CESIUM_BASE_URL = `${APP_BASE_PATH}${CESIUM_PATHNAME}`;
