f032c804ac3014d372e283154861701c75aa8722
David Glogaza <david.glogaza40@googlemail.com>
2024-08-21 09:25:49 +0200

    remove unused imports

--- playgrounds/map-control-playground/src/app/App.jsx
+++ playgrounds/map-control-playground/src/app/App.jsx
@@ -1,62 +1,52 @@
-import React, { useEffect, useContext, useState, useRef } from 'react';
-import { MappingConstants } from 'react-cismap';
-import TopicMapContextProvider from 'react-cismap/contexts/TopicMapContextProvider';
-import GazetteerSearchComponent from 'react-cismap/GazetteerSearchComponent';
-import convertItemToFeature from './helper/convertItemToFeature';
-import { TopicMapContext } from 'react-cismap/contexts/TopicMapContextProvider';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'leaflet/dist/leaflet.css';
-import 'react-bootstrap-typeahead/css/Typeahead.css';
-import 'react-cismap/topicMaps.css';
-import './App.css';
-import Map from './Map';
+import {
+  MenuOutlined,
+  MinusOutlined,
+  PlusOutlined,
+  SettingFilled,
+  ShrinkOutlined,
+} from "@ant-design/icons";
+import {
+  Control,
+  ControlButtonStyler,
+  ControlLayout,
+  Main,
+} from "@carma-mapping/map-controls-layout";
+import { Divider } from "antd";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "leaflet.locatecontrol";
+import "leaflet.locatecontrol/dist/L.Control.Locate.css";
+import "leaflet/dist/leaflet.css";
+import { useEffect, useRef, useState } from "react";
+import "react-bootstrap-typeahead/css/Typeahead.css";
+import { MappingConstants } from "react-cismap";
+import TopicMapContextProvider from "react-cismap/contexts/TopicMapContextProvider";
+import GazetteerSearchComponent from "react-cismap/GazetteerSearchComponent";
+import "react-cismap/topicMaps.css";
+import "./App.css";
+import convertItemToFeature from "./helper/convertItemToFeature";
 import {
   getFeatureStyler,
   getPoiClusterIconCreatorFunction,
-} from './helper/styler';
-import './index.css';
-import 'leaflet.locatecontrol/dist/L.Control.Locate.css';
-import 'leaflet.locatecontrol';
-import {
-  ControlLayout,
-  Control,
-  Main,
-  ControlButtonStyler,
-  ControlCenterStyler,
-} from '@carma-mapping/map-controls-layout';
-import {
-  AimOutlined,
-  HomeOutlined,
-  LoadingOutlined,
-  SettingFilled,
-  SyncOutlined,
-  ShrinkOutlined,
-  MenuOutlined,
-  PlusOutlined,
-  MinusOutlined,
-} from '@ant-design/icons';
-import GoogleMapIframe from './components/GoogleMapIframe';
-import DemoExcalidraw from './components/DemoExcalidraw';
-import InfoBox from 'react-cismap/topicmaps/InfoBox';
-import GenericInfoBoxFromFeature from 'react-cismap/topicmaps/GenericInfoBoxFromFeature';
-import { Divider } from 'antd';
+} from "./helper/styler";
+import "./index.css";
+import Map from "./Map";
 
-if (typeof global === 'undefined') {
+if (typeof global === "undefined") {
   window.global = window;
 }
 
 const iconPadding = {
-  backgroundColor: '#fff',
-  border: '2px solid rgba(0, 0, 0, .23)',
-  borderRadius: '4px',
-  width: '34px',
-  height: '34px',
-  textAlign: 'center',
-  cursor: 'pointer',
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
-  fontSize: '18px',
+  backgroundColor: "#fff",
+  border: "2px solid rgba(0, 0, 0, .23)",
+  borderRadius: "4px",
+  width: "34px",
+  height: "34px",
+  textAlign: "center",
+  cursor: "pointer",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  fontSize: "18px",
 };
 
 function App() {
@@ -66,7 +56,7 @@ function App() {
   const [layoutHeight, setLayoutHeight] = useState(null);
 
   useEffect(() => {
-    document.title = 'Map Wuppertal';
+    document.title = "Map Wuppertal";
   }, []);
 
   useEffect(() => {
@@ -82,7 +72,7 @@ function App() {
     <TopicMapContextProvider
       appKey="OnlineBaederkarteWuppertal2022"
       featureItemsURL={
-        'https://wupp-topicmaps-data.cismet.de/data/baeder.data.json'
+        "https://wupp-topicmaps-data.cismet.de/data/baeder.data.json"
       }
       referenceSystemDefinition={MappingConstants.proj4crs25832def}
       mapEPSGCode="25832"
@@ -106,8 +96,8 @@ function App() {
             <PlusOutlined />
             <Divider
               style={{
-                marginTop: '0px',
-                marginBottom: '0px',
+                marginTop: "0px",
+                marginBottom: "0px",
               }}
             />
             <MinusOutlined />
@@ -132,14 +122,14 @@ function App() {
           <div
             style={{
               ...iconPadding,
-              fontSize: '14px',
+              fontSize: "14px",
               opacity: 0.9,
-              width: '100%',
-              maxWidth: '100%',
-              height: '100%',
-              padding: '5px 4px',
-              border: 'none',
-              borderRadius: '0px',
+              width: "100%",
+              maxWidth: "100%",
+              height: "100%",
+              padding: "5px 4px",
+              border: "none",
+              borderRadius: "0px",
             }}
           >
             <div>Center controller</div>
@@ -151,12 +141,12 @@ function App() {
         <Control position="bottomright" order={10} fullCollapseWidth={true}>
           <div
             style={{
-              width: '300px',
-              background: 'white',
-              height: '80px',
-              padding: '4px',
-              fontSize: '12px',
-              opacity: '0.9',
+              width: "300px",
+              background: "white",
+              height: "80px",
+              padding: "4px",
+              fontSize: "12px",
+              opacity: "0.9",
             }}
           >
             Info Box
