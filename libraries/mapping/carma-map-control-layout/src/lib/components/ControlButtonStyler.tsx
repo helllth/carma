cff80dd3310ef4dbec056acb72e399aa0d4c777a
David Glogaza <david.glogaza40@googlemail.com>
2024-08-21 09:25:13 +0200

    add more props

--- libraries/mapping/carma-map-control-layout/src/lib/components/ControlButtonStyler.tsx
+++ libraries/mapping/carma-map-control-layout/src/lib/components/ControlButtonStyler.tsx
@@ -1,6 +1,7 @@
-import React, { CSSProperties, ReactNode } from 'react';
+import React, { CSSProperties, ReactNode } from "react";
 
-interface ControlButtonStylerProps {
+interface ControlButtonStylerProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   children: ReactNode;
   width?: string;
   height?: string;
@@ -10,28 +11,33 @@ interface ControlButtonStylerProps {
 
 const ControlButtonStyler: React.FC<ControlButtonStylerProps> = ({
   children,
-  width = '34px',
-  height = '34px',
-  fontSize = '18px',
+  width = "34px",
+  height = "34px",
+  fontSize = "18px",
   singleButtonClick = () => {},
+  ...props
 }) => {
   const iconPadding = {
-    backgroundColor: '#fff',
-    border: '2px solid rgba(0, 0, 0, .3)',
-    borderRadius: '4px',
+    backgroundColor: "#fff",
+    border: "2px solid rgba(0, 0, 0, .3)",
+    borderRadius: "4px",
     width,
     height,
-    textAlign: 'center', 
-    cursor: 'pointer',
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'space-evenly',
+    textAlign: "center",
+    cursor: "pointer",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "space-evenly",
     fontSize,
     // fontWeight: 700,
   } as CSSProperties;
   return (
-    <button style={iconPadding} onClick={singleButtonClick}>
+    <button
+      className="text-center flex flex-col text-red-500 justify-center items-center w-60 h-60"
+      onClick={singleButtonClick}
+      {...props}
+    >
       {children}
     </button>
   );
