1f9ef650b547926d81ec70b3b110a4452674995e
Thorsten Hell <thorsten.hell@cismet.de>
2024-08-22 14:03:53 +0200

    integrate version in dev-hot-reload

--- envirometrics/wuppertal/floodingmap/src/version.js
+++ envirometrics/wuppertal/floodingmap/src/version.js
@@ -1,17 +1,18 @@
-const APPVERSION = '%APPLICATION_VERSION%';
-const APPHASH = '#%APPLICATION_HASH%';
+const APPVERSION = "%APPLICATION_VERSION%";
+const APPHASH = "#%APPLICATION_HASH%";
+import versionData from "./version.json";
 
 export const getApplicationVersion = () => {
   /*eslint-disable no-useless-concat*/
-  if (APPVERSION === '%APPLICATION' + '_' + 'VERSION%') {
-    return 'dev-hot-reload';
+  if (APPVERSION === "%APPLICATION" + "_" + "VERSION%") {
+    return versionData.version + " (dev-hot-reload)";
   } else {
     return APPVERSION;
   }
 };
 export const getApplicationHash = () => {
-  if (APPHASH === '%APPLICATION' + '_' + 'HASH%') {
-    return '#dev-hot-reload';
+  if (APPHASH === "%APPLICATION" + "_" + "HASH%") {
+    return "#dev-hot-reload";
   } else {
     return APPHASH;
   }
