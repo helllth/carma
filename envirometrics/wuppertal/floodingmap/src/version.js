c91f09bf46c7410ae99789cb025975a684a3855e
Thorsten Hell <thorsten.hell@cismet.de>
2024-08-22 15:32:24 +0200

    ditch getApplicationHash and update getApplicationVersion

--- envirometrics/wuppertal/floodingmap/src/version.js
+++ envirometrics/wuppertal/floodingmap/src/version.js
@@ -5,17 +5,10 @@ import versionData from "./version.json";
 export const getApplicationVersion = () => {
   let v = versionData.version;
   if (versionData.triggered === "live") {
-    return v;
-  } else if (versionData.triggered === "dev") {
-    return "dev " + v + " (" + versionData.md5.substring(0, 4) + ")";
+    return "v" + v;
+  } else if (versionData.triggered) {
+    return versionData.triggered + " v" + v + "++ (" + versionData.md5.substring(0, 4) + ")";
   } else {
-    return v + " (dev-hot-reload)";
-  }
-};
-export const getApplicationHash = () => {
-  if (APPHASH === "%APPLICATION" + "_" + "HASH%") {
-    return "#dev-hot-reload";
-  } else {
-    return APPHASH;
+    return "v" + v + "++ (dev-hot-reload)";
   }
 };
