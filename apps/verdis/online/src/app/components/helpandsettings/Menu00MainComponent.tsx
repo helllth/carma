ff2b3f83b935e827c338b672f56d6303716f4a3f
PavelOlkhovoi <p-olhovoi@bk.ru>
2024-08-28 14:30:52 +0200

    replace modal menu

--- apps/verdis/online/src/app/components/helpandsettings/Menu00MainComponent.tsx
+++ apps/verdis/online/src/app/components/helpandsettings/Menu00MainComponent.tsx
@@ -1,87 +1,47 @@
-import ModalApplicationMenu from 'react-cismap/topicmaps/menu/ModalApplicationMenu';
-import Section from 'react-cismap/topicmaps/menu/Section';
-import ConfigurableDocBlocks from 'react-cismap/topicmaps/ConfigurableDocBlocks';
-import Introduction from './Menu05Introduction';
-import { getUiState, showSettings } from '../../../store/slices/ui';
-import { useDispatch, useSelector } from 'react-redux';
-import Menu10Datengrundlagen from './Menu10Datengrundlagen';
-import Menu20MeinKassenzeichen from './Menu20MeinKassenzeichen';
-import Menu40Anleitung from './Menu40Anleitung';
-import Menu41Mailservice from './Menu41Mailservice';
-import Menu42Aenderungen from './Menu42Aenderungen';
-import Menu60Datenschutz from './Menu60Datenschutz';
-import Menu50FAQ from './Menu50FAQ';
-import Menu30Kartenhintergruende from './Menu30Kartenhintergruende';
-import Menu99Footer from './Menu99Footer';
+import ModalApplicationMenu from "react-cismap/topicmaps/menu/ModalApplicationMenu";
+import Section from "react-cismap/topicmaps/menu/Section";
+import { getUiState, showSettings } from "../../../store/slices/ui";
+import { useDispatch, useSelector } from "react-redux";
+import Menu99Footer from "./Menu99Footer";
+import {
+  modalMenuTitleText,
+  Introduction,
+  Menu10Datengrundlagen,
+  Menu20MeinKassenzeichen,
+  Menu30KartenhintergruendeText,
+  Menu40Anleitung,
+  Menu41Mailservice,
+  Menu42Aenderungen,
+  Menu50FAQ,
+  Menu60Datenschutz,
+} from "@carma-collab/wuppertal/verdis-online";
 
 const ModalHelpAndInfo = () => {
   const uiState = useSelector(getUiState);
   const dispatch = useDispatch();
   return (
     <ModalApplicationMenu
-      menuIcon={'info'}
-      menuTitle={'Kompaktanleitung und Hintergrundinformationen'}
+      menuIcon={"info"}
+      menuTitle={modalMenuTitleText}
       menuFooter={<Menu99Footer />}
       menuIntroduction={<Introduction />}
       visible={uiState.settingsVisible}
       setVisible={(value) => dispatch(showSettings({ visible: value }))}
       menuSections={[
-        <Section
-          key="datengrundlage"
-          sectionKey="datengrundlage"
-          sectionTitle="Grundlagen der Datenerhebung"
-          sectionBsStyle="warning"
-          sectionContent={<Menu10Datengrundlagen />}
-        />,
-        <Section
-          key="meinKassenzeichen"
-          sectionKey="meinKassenzeichen"
-          sectionTitle="Mein Kassenzeichen"
-          sectionBsStyle="warning"
-          sectionContent={<Menu20MeinKassenzeichen />}
-        />,
+        <Menu10Datengrundlagen />,
+        <Menu20MeinKassenzeichen />,
         <Section
           key="kartenhintergruende"
           sectionKey="kartenhintergruende"
           sectionTitle="Hintergrundkarten"
           sectionBsStyle="info"
-          sectionContent={<Menu30Kartenhintergruende />}
+          sectionContent={<Menu30KartenhintergruendeText />}
         />,
-        <Section
-          key="anleitung"
-          sectionKey="anleitung"
-          sectionTitle="Kurzanleitung"
-          sectionBsStyle="success"
-          sectionContent={<Menu40Anleitung />}
-        />,
-        <Section
-          key="mailservice"
-          sectionKey="mailservice"
-          sectionTitle="Mailservice"
-          sectionBsStyle="info"
-          sectionContent={<Menu41Mailservice />}
-        />,
-        <Section
-          key="aenderungen"
-          sectionKey="aenderungen"
-          sectionTitle="Änderungen vornehmen und übermitteln"
-          sectionBsStyle="success"
-          sectionContent={<Menu42Aenderungen />}
-        />,
-        <Section
-          key="faq"
-          sectionKey="faq"
-          sectionTitle="Häufig gestellte Fragen"
-          sectionBsStyle="success"
-          sectionContent={<Menu50FAQ />}
-        />,
-        <Section
-          key="datenschutz"
-          sectionKey="datenschutz"
-          sectionTitle="Datenschutz"
-          sectionBsStyle="danger"
-          sectionContent={<Menu60Datenschutz />}
-        />,
+        <Menu40Anleitung />,
+        <Menu41Mailservice />,
+        <Menu42Aenderungen />,
+        <Menu50FAQ />,
+        <Menu60Datenschutz />,
       ]}
     />
   );
--- apps/verdis/online/src/app/components/helpandsettings/Menu30Kartenhintergruende.tsx
+++
@@ -1,5 +0,0 @@
-const Menu30Kartenhintergruende = () => {
-  return <div>Menu30Kartenhintergruende</div>;
-};
-
-export default Menu30Kartenhintergruende;
