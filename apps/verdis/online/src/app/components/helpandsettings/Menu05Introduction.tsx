b58d43764bea8bd371e868aa5c5b6bf8851ad588
PavelOlkhovoi <p-olhovoi@bk.ru>
2024-08-28 14:30:52 +0200

    replace modal menu

--- apps/verdis/online/src/app/components/helpandsettings/Menu05Introduction.tsx
+++
@@ -1,9 +0,0 @@
-const Section = () => {
-  return (
-    <span>
-      Bitte wählen Sie eine der folgenden farbigen Schaltflächen, um sich
-      weitere Informationen zu dem entsprechenden Thema anzeigen zu lassen:
-    </span>
-  );
-};
-export default Section;
