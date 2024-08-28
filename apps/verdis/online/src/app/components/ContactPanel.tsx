6ac6e569799405bd86fb5617490c923005b3272a
PavelOlkhovoi <p-olhovoi@bk.ru>
2024-08-28 15:02:44 +0200

    replace texts titles

--- apps/verdis/online/src/app/components/ContactPanel.tsx
+++ apps/verdis/online/src/app/components/ContactPanel.tsx
@@ -1,3 +1,4 @@
+import { panelTitles } from "@carma-collab/wuppertal/verdis-online";
 interface ContactProps {
   contact?: any;
 }
@@ -6,28 +7,28 @@ const ContactPanel = ({ contact }: ContactProps) => {
   let _contact;
   if (!contact) {
     _contact = {
-      anrede: 'Herr',
-      vorname: 'Dirk',
-      nachname: 'Steinbacher',
-      telefon: '+49-202-563 5794',
-      mail: 'dirk.steinbacher@stadt.wuppertal.de',
-      image: 'SteinbacherD102.png',
+      anrede: "Herr",
+      vorname: "Dirk",
+      nachname: "Steinbacher",
+      telefon: "+49-202-563 5794",
+      mail: "dirk.steinbacher@stadt.wuppertal.de",
+      image: "SteinbacherD102.png",
     };
   } else {
     _contact = contact;
   }
 
   let styleOverride = {
-    marginBottom: '5px',
-    width: '100%',
+    marginBottom: "5px",
+    width: "100%",
   };
 
   if (_contact.image) {
     styleOverride = Object.assign(styleOverride, {
       backgroundImage: 'url("/images/contacts/' + _contact.image + '")',
-      backgroundRepeat: 'no-repeat',
-      backgroundSize: '64px 64px',
-      backgroundPosition: 'right 10px top 15px',
+      backgroundRepeat: "no-repeat",
+      backgroundSize: "64px 64px",
+      backgroundPosition: "right 10px top 15px",
     });
   }
 
@@ -36,22 +37,22 @@ const ContactPanel = ({ contact }: ContactProps) => {
       style={{
         ...styleOverride,
         minHeight: 20,
-        backgroundColor: '#f5f5f5',
-        border: '1px solid #e3e3e3',
+        backgroundColor: "#f5f5f5",
+        border: "1px solid #e3e3e3",
         padding: 9,
         borderRadius: 3,
-        height: 'auto',
+        height: "auto",
       }}
     >
-      <h4>Ihr Ansprechpartner:</h4>
+      <h4>{panelTitles.contactTitle}</h4>
       <h5>
-        {_contact.anrede} {_contact.vorname} {_contact.nachname}{' '}
+        {_contact.anrede} {_contact.vorname} {_contact.nachname}{" "}
       </h5>
       <h6>
-        Telefon: <a href={'tel:' + _contact.telefon}>{_contact.telefon} </a>
+        Telefon: <a href={"tel:" + _contact.telefon}>{_contact.telefon} </a>
       </h6>
       <h6>
-        eMail: <a href={'mailto:' + _contact.mail}>{_contact.mail} </a>
+        eMail: <a href={"mailto:" + _contact.mail}>{_contact.mail} </a>
       </h6>
     </div>
   );
--- apps/verdis/online/src/app/components/KassenzeichenPanel.tsx
+++ apps/verdis/online/src/app/components/KassenzeichenPanel.tsx
@@ -1,14 +1,15 @@
-import { useSelector } from 'react-redux';
-import { getKassenzeichen } from '../../store/slices/kassenzeichen';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faLockOpen } from '@fortawesome/free-solid-svg-icons';
+import { useSelector } from "react-redux";
+import { getKassenzeichen } from "../../store/slices/kassenzeichen";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLockOpen } from "@fortawesome/free-solid-svg-icons";
+import { panelTitles } from "@carma-collab/wuppertal/verdis-online";
 
 const KassenzeichenPanel = () => {
   const kassenzeichen = useSelector(getKassenzeichen);
 
   const styleOverride = {
-    marginBottom: '5px',
-    width: '100%',
+    marginBottom: "5px",
+    width: "100%",
   };
   return (
     <div
@@ -16,22 +17,22 @@ const KassenzeichenPanel = () => {
       style={{
         ...styleOverride,
         minHeight: 20,
-        backgroundColor: '#f5f5f5',
-        border: '1px solid #e3e3e3',
+        backgroundColor: "#f5f5f5",
+        border: "1px solid #e3e3e3",
         padding: 9,
         borderRadius: 3,
-        height: 'auto',
+        height: "auto",
       }}
     >
-      <h4>Kassenzeichen</h4>
+      <h4>{panelTitles.kassenzeichenTitle}</h4>
       <small>erfasst am {kassenzeichen.datum_erfassung}</small>
-      <table style={{ width: '100%' }}>
+      <table style={{ width: "100%" }}>
         <tbody>
           <tr>
             <td>
               <h2>{kassenzeichen.kassenzeichennummer8}</h2>
             </td>
-            <td style={{ textAlign: 'right', verticalAlign: 'bottom' }}>
+            <td style={{ textAlign: "right", verticalAlign: "bottom" }}>
               <FontAwesomeIcon icon={faLockOpen} />
             </td>
           </tr>
