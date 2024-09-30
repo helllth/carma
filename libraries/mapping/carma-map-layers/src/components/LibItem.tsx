a083359b59dce662f25e5eb4aa91ee17ac13c5f5
David Glogaza <david.glogaza40@googlemail.com>
2024-09-30 09:53:58 +0200

    show whole description on click

--- libraries/mapping/carma-map-layers/src/components/LibItem.tsx
+++ libraries/mapping/carma-map-layers/src/components/LibItem.tsx
@@ -49,6 +49,7 @@ const LibItem = ({
   const [collectionImages, setCollectionImages] = useState<string[]>([]);
   const [openDeleteModal, setOpenDeleteModal] = useState(false);
   const [forceWMS, setForceWMS] = useState(false);
+  const [openInfo, setOpenInfo] = useState(false);
   const title = layer.title;
   const description = layer.description;
   const keywords = layer.keywords;
@@ -237,257 +238,267 @@ const LibItem = ({
   }, []);
 
   return (
-    <div
-      className="flex flex-col rounded-lg w-full shadow-sm h-fit hover:!shadow-lg bg-white"
-      onMouseEnter={() => setHovered(true)}
-      onMouseLeave={() => setHovered(false)}
-    >
-      <div className="relative overflow-hidden isolate rounded-md flex justify-center items-center w-full aspect-[1.7777/1]">
-        {isLoading && (
-          <div style={{ position: "absolute", left: "50%" }}>
-            <Spin />
-          </div>
-        )}
+    <>
+      <div
+        className="flex flex-col rounded-lg w-full shadow-sm h-fit hover:!shadow-lg bg-white"
+        onMouseEnter={() => setHovered(true)}
+        onMouseLeave={() => setHovered(false)}
+        onClick={() => setOpenInfo(!openInfo)}
+      >
+        <div className="relative overflow-hidden isolate rounded-md flex justify-center items-center w-full aspect-[1.7777/1]">
+          {isLoading && (
+            <div style={{ position: "absolute", left: "50%" }}>
+              <Spin />
+            </div>
+          )}
 
-        {(thumbUrl && layer.type !== "collection") || layer.thumbnail ? (
-          <img
-            src={layer.thumbnail ? layer.thumbnail : thumbUrl}
-            alt={title}
-            loading="lazy"
-            className={`object-cover relative h-full overflow-clip w-[calc(130%+7.2px)] ${
-              hovered && "scale-110"
-            } transition-all duration-200`}
-            onLoad={(e) => {
-              setIsLoading(false);
-            }}
-          />
-        ) : layer.type === "collection" ? (
-          <div
-            className={`overflow-clip ${
-              collectionImages.length > 3
-                ? "grid grid-cols-2"
-                : "flex flex-col h-full"
-            }`}
-          >
-            {collectionImages.map((imgUrl, i) => {
-              return (
-                <img
-                  key={`collection_img_${i}`}
-                  src={imgUrl}
-                  alt={title}
-                  loading="lazy"
-                  className={`object-cover relative overflow-clip w-[calc(130%+7.2px)] ${
-                    hovered && "scale-110"
-                  } transition-all duration-200`}
-                />
-              );
-            })}
-          </div>
-        ) : (
-          <div className="object-cover relative h-full overflow-clip w-[calc(130%+7.2px)]" />
-        )}
-
-        {layer.type !== "collection" ? (
-          isFavorite ? (
-            <FontAwesomeIcon
-              className="absolute right-1 top-1 text-3xl text-yellow-200 cursor-pointer z-50"
-              icon={faStar}
-              onClick={() => {
-                if (removeFavorite) {
-                  removeFavorite(layer);
-                }
+          {(thumbUrl && layer.type !== "collection") || layer.thumbnail ? (
+            <img
+              src={layer.thumbnail ? layer.thumbnail : thumbUrl}
+              alt={title}
+              loading="lazy"
+              className={`object-cover relative h-full overflow-clip w-[calc(130%+7.2px)] ${
+                hovered && "scale-110"
+              } transition-all duration-200`}
+              onLoad={(e) => {
+                setIsLoading(false);
               }}
             />
+          ) : layer.type === "collection" ? (
+            <div
+              className={`overflow-clip ${
+                collectionImages.length > 3
+                  ? "grid grid-cols-2"
+                  : "flex flex-col h-full"
+              }`}
+            >
+              {collectionImages.map((imgUrl, i) => {
+                return (
+                  <img
+                    key={`collection_img_${i}`}
+                    src={imgUrl}
+                    alt={title}
+                    loading="lazy"
+                    className={`object-cover relative overflow-clip w-[calc(130%+7.2px)] ${
+                      hovered && "scale-110"
+                    } transition-all duration-200`}
+                  />
+                );
+              })}
+            </div>
+          ) : (
+            <div className="object-cover relative h-full overflow-clip w-[calc(130%+7.2px)]" />
+          )}
+
+          {layer.type !== "collection" ? (
+            isFavorite ? (
+              <FontAwesomeIcon
+                className="absolute right-1 top-1 text-3xl text-yellow-200 cursor-pointer z-50"
+                icon={faStar}
+                onClick={() => {
+                  if (removeFavorite) {
+                    removeFavorite(layer);
+                  }
+                }}
+              />
+            ) : (
+              <FontAwesomeIcon
+                className="absolute right-1 top-1 text-3xl cursor-pointer z-50 text-white drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,1)]"
+                icon={regularFaStar}
+                onClick={() => {
+                  if (addFavorite) {
+                    addFavorite(layer);
+                  }
+                }}
+              />
+              // <StarOutlined className="absolute right-1 top-1 text-3xl text-white drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.9)]" />
+            )
+          ) : null}
+          {layer.type === "link" ? (
+            <a
+              className="absolute left-1 top-1 text-3xl cursor-pointer z-50 text-white drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,1)]"
+              href={layer.url}
+              target="topicMaps"
+            >
+              <FontAwesomeIcon icon={faExternalLinkAlt} />
+            </a>
+          ) : layer.type === "collection" ? (
+            <>
+              <FontAwesomeIcon
+                onClick={handleLayerClick}
+                icon={faSquareUpRight}
+                className="absolute left-1 top-1 text-3xl cursor-pointer text-white drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,1)] z-50"
+              />
+              <FontAwesomeIcon
+                onClick={() => setOpenDeleteModal(true)}
+                icon={faTrash}
+                className="absolute left-1 top-11 text-3xl cursor-pointer text-white drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,1)] z-50"
+              />
+            </>
           ) : (
             <FontAwesomeIcon
-              className="absolute right-1 top-1 text-3xl cursor-pointer z-50 text-white drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,1)]"
-              icon={regularFaStar}
-              onClick={() => {
-                if (addFavorite) {
-                  addFavorite(layer);
-                }
-              }}
-            />
-            // <StarOutlined className="absolute right-1 top-1 text-3xl text-white drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.9)]" />
-          )
-        ) : null}
-        {layer.type === "link" ? (
-          <a
-            className="absolute left-1 top-1 text-3xl cursor-pointer z-50 text-white drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,1)]"
-            href={layer.url}
-            target="topicMaps"
-          >
-            <FontAwesomeIcon icon={faExternalLinkAlt} />
-          </a>
-        ) : layer.type === "collection" ? (
-          <>
-            <FontAwesomeIcon
               onClick={handleLayerClick}
-              icon={faSquareUpRight}
+              icon={isActiveLayer ? faMinus : faPlus}
               className="absolute left-1 top-1 text-3xl cursor-pointer text-white drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,1)] z-50"
             />
-            <FontAwesomeIcon
-              onClick={() => setOpenDeleteModal(true)}
-              icon={faTrash}
-              className="absolute left-1 top-11 text-3xl cursor-pointer text-white drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,1)] z-50"
-            />
-          </>
-        ) : (
-          <FontAwesomeIcon
-            onClick={handleLayerClick}
-            icon={isActiveLayer ? faMinus : faPlus}
-            className="absolute left-1 top-1 text-3xl cursor-pointer text-white drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,1)] z-50"
-          />
-        )}
-        <InfoOutlined className="absolute right-1 bottom-1 text-3xl cursor-pointer text-white drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,1)] z-50" />
-        {hovered && (
-          <div className="flex flex-col items-center gap-2 absolute top-0 w-full h-full justify-center p-8 px-10">
-            {layer.type === "link" ? (
-              <a
-                className="w-36 bg-gray-100 hover:no-underline text-black hover:text-neutral-600 hover:bg-gray-50 rounded-md py-2 flex text-center items-center px-2"
-                href={layer.url}
-                target="topicMaps"
-              >
+          )}
+          <InfoOutlined className="absolute right-1 bottom-1 text-3xl cursor-pointer text-white drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,1)] z-50" />
+          {hovered && (
+            <div className="flex flex-col items-center gap-2 absolute top-0 w-full h-full justify-center p-8 px-10">
+              {layer.type === "link" ? (
+                <a
+                  className="w-36 bg-gray-100 hover:no-underline text-black hover:text-neutral-600 hover:bg-gray-50 rounded-md py-2 flex text-center items-center px-2"
+                  href={layer.url}
+                  target="topicMaps"
+                >
+                  <>
+                    <FontAwesomeIcon
+                      icon={faExternalLinkAlt}
+                      className="text-lg mr-2"
+                    />
+                    Öffnen
+                  </>
+                </a>
+              ) : layer.type === "collection" ? (
                 <>
-                  <FontAwesomeIcon
-                    icon={faExternalLinkAlt}
-                    className="text-lg mr-2"
-                  />
-                  Öffnen
+                  <button
+                    className="w-36 bg-gray-100 hover:bg-gray-50 rounded-md py-2 flex text-center items-center px-2"
+                    onClick={handleLayerClick}
+                  >
+                    <FontAwesomeIcon
+                      icon={faSquareUpRight}
+                      className="text-lg mr-2"
+                    />{" "}
+                    Anwenden
+                  </button>
+                  <button
+                    className="w-36 bg-gray-100 hover:bg-gray-50 rounded-md py-2 flex text-center items-center px-2"
+                    onClick={() => setOpenDeleteModal(true)}
+                  >
+                    <FontAwesomeIcon icon={faTrash} className="text-lg mr-2" />{" "}
+                    Löschen
+                  </button>
                 </>
-              </a>
-            ) : layer.type === "collection" ? (
-              <>
+              ) : (
                 <button
                   className="w-36 bg-gray-100 hover:bg-gray-50 rounded-md py-2 flex text-center items-center px-2"
                   onClick={handleLayerClick}
                 >
-                  <FontAwesomeIcon
-                    icon={faSquareUpRight}
-                    className="text-lg mr-2"
-                  />{" "}
-                  Anwenden
-                </button>
-                <button
-                  className="w-36 bg-gray-100 hover:bg-gray-50 rounded-md py-2 flex text-center items-center px-2"
-                  onClick={() => setOpenDeleteModal(true)}
-                >
-                  <FontAwesomeIcon icon={faTrash} className="text-lg mr-2" />{" "}
-                  Löschen
-                </button>
-              </>
-            ) : (
-              <button
-                className="w-36 bg-gray-100 hover:bg-gray-50 rounded-md py-2 flex text-center items-center px-2"
-                onClick={handleLayerClick}
-              >
-                {isActiveLayer ? (
-                  <>
-                    <FontAwesomeIcon
-                      icon={faCircleMinus}
-                      className="text-lg mr-2"
-                    />{" "}
-                    Entfernen
-                  </>
-                ) : (
-                  <>
-                    <FontAwesomeIcon
-                      icon={faCirclePlus}
-                      className="text-lg mr-2"
-                    />{" "}
-                    Hinzufügen
-                  </>
-                )}
-              </button>
-            )}
-            <button
-              disabled
-              className="w-36 flex text-center items-center px-2 bg-gray-100 hover:bg-gray-50 rounded-md py-2 cursor-not-allowed"
-            >
-              <FontAwesomeIcon icon={faCircleInfo} className="mr-2 text-lg" />
-              Informationen
-            </button>
-          </div>
-        )}
-      </div>
-      <div className="flex flex-col gap-2 p-4">
-        <div className="w-full flex items-center gap-2">
-          <h3 className="text-lg truncate w-full mb-0">
-            {hightlightTextIndexes ? (
-              <>
-                {title.substring(0, hightlightTextIndexes[0])}
-                <span
-                  style={{
-                    backgroundColor: "rgba(240, 215, 139, 0.8)",
-                    padding: "0 0.08em",
-                  }}
-                >
-                  {title.substring(
-                    hightlightTextIndexes[0],
-                    hightlightTextIndexes[1] + 1,
+                  {isActiveLayer ? (
+                    <>
+                      <FontAwesomeIcon
+                        icon={faCircleMinus}
+                        className="text-lg mr-2"
+                      />{" "}
+                      Entfernen
+                    </>
+                  ) : (
+                    <>
+                      <FontAwesomeIcon
+                        icon={faCirclePlus}
+                        className="text-lg mr-2"
+                      />{" "}
+                      Hinzufügen
+                    </>
                   )}
-                </span>
-
-                {title.substring(hightlightTextIndexes[1] + 1)}
-              </>
-            ) : (
-              title
-            )}
-          </h3>
-          {keywords &&
-            extractVectorStyles(keywords)?.vectorStyle &&
-            !forceWMS && (
-              <FontAwesomeIcon
-                icon={faRocket}
-                className="text-xl cursor-pointer text-gray-700 z-50"
-              />
-            )}
+                </button>
+              )}
+              <button
+                disabled
+                className="w-36 flex text-center items-center px-2 bg-gray-100 hover:bg-gray-50 rounded-md py-2 cursor-not-allowed"
+              >
+                <FontAwesomeIcon icon={faCircleInfo} className="mr-2 text-lg" />
+                Informationen
+              </button>
+            </div>
+          )}
         </div>
-        <p
-          className="text-base line-clamp-3 h-[66px]"
-          style={{ color: "rgba(0,0,0,0.7)" }}
-        >
-          {match && match.length > 1 ? match[1].trim() : description}
-        </p>
-        <p
-          style={{ color: "rgba(0,0,0,0.5)", fontSize: "0.875rem" }}
-          className="mb-0 h-10 line-clamp-2"
-        >
-          {tags?.map((tag, i) => (
-            <span key={"tag_" + tag + "_" + i}>
-              <span>{tag}</span>
-              {i + 1 < tags.length && <span> · </span>}
-            </span>
-          ))}
-        </p>
-      </div>
-      <Modal
-        footer={null}
-        open={openDeleteModal}
-        onCancel={() => setOpenDeleteModal(false)}
-      >
         <div className="flex flex-col gap-2 p-4">
-          <h3 className="text-lg">
-            Zusammenstellung {title} wirklich löschen?
-          </h3>
-          <p className="text-base line-clamp-3 h-[66px]">
-            Diese Aktion kann nicht rückgängig gemacht werden.
-          </p>
-          <div className="flex gap-2 w-full justify-end items-center">
-            <Button onClick={() => setOpenDeleteModal(false)}>Abbrechen</Button>
-            <Button
-              danger
-              onClick={() => {
-                setOpenDeleteModal(false);
-                setAdditionalLayers(layer, true);
-              }}
-            >
-              Löschen
-            </Button>
+          <div className="w-full flex items-center gap-2">
+            <h3 className="text-lg truncate w-full mb-0">
+              {hightlightTextIndexes ? (
+                <>
+                  {title.substring(0, hightlightTextIndexes[0])}
+                  <span
+                    style={{
+                      backgroundColor: "rgba(240, 215, 139, 0.8)",
+                      padding: "0 0.08em",
+                    }}
+                  >
+                    {title.substring(
+                      hightlightTextIndexes[0],
+                      hightlightTextIndexes[1] + 1,
+                    )}
+                  </span>
+
+                  {title.substring(hightlightTextIndexes[1] + 1)}
+                </>
+              ) : (
+                title
+              )}
+            </h3>
+            {keywords &&
+              extractVectorStyles(keywords)?.vectorStyle &&
+              !forceWMS && (
+                <FontAwesomeIcon
+                  icon={faRocket}
+                  className="text-xl cursor-pointer text-gray-700 z-50"
+                />
+              )}
           </div>
+          <p
+            className="text-base line-clamp-3 h-[66px]"
+            style={{ color: "rgba(0,0,0,0.7)" }}
+          >
+            {match && match.length > 1 ? match[1].trim() : description}
+          </p>
+          <p
+            style={{ color: "rgba(0,0,0,0.5)", fontSize: "0.875rem" }}
+            className="mb-0 h-10 line-clamp-2"
+          >
+            {tags?.map((tag, i) => (
+              <span key={"tag_" + tag + "_" + i}>
+                <span>{tag}</span>
+                {i + 1 < tags.length && <span> · </span>}
+              </span>
+            ))}
+          </p>
         </div>
-      </Modal>
-    </div>
+        <Modal
+          footer={null}
+          open={openDeleteModal}
+          onCancel={() => setOpenDeleteModal(false)}
+        >
+          <div className="flex flex-col gap-2 p-4">
+            <h3 className="text-lg">
+              Zusammenstellung {title} wirklich löschen?
+            </h3>
+            <p className="text-base line-clamp-3 h-[66px]">
+              Diese Aktion kann nicht rückgängig gemacht werden.
+            </p>
+            <div className="flex gap-2 w-full justify-end items-center">
+              <Button onClick={() => setOpenDeleteModal(false)}>
+                Abbrechen
+              </Button>
+              <Button
+                danger
+                onClick={() => {
+                  setOpenDeleteModal(false);
+                  setAdditionalLayers(layer, true);
+                }}
+              >
+                Löschen
+              </Button>
+            </div>
+          </div>
+        </Modal>
+      </div>
+      {openInfo && (
+        <div className="w-full h-80 shadow-md bg-white col-span-full">
+          <p className="text-lg p-4">{layer.description}</p>
+        </div>
+      )}
+    </>
   );
 };
 
