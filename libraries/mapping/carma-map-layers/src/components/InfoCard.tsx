f1c72dbf134d6c9c5d3f942916af70038f132a25
David Glogaza <david.glogaza40@googlemail.com>
2024-10-02 10:51:56 +0200

    fix overflow issues

--- libraries/mapping/carma-map-layers/src/components/InfoCard.tsx
+++ libraries/mapping/carma-map-layers/src/components/InfoCard.tsx
@@ -12,40 +12,44 @@ const InfoCard = ({ layer }: InfoCardProps) => {
   return (
     <div className="w-full h-[400px] p-6 shadow-sm hover:!shadow-lg rounded-lg bg-blue-50 col-span-full">
       <div className="flex h-full flex-col justify-between">
-        <div className="flex pb-4 gap-2 items-center">
+        <div className="flex pb-4 gap-4 items-center">
           <h3>{title}</h3>
-          <p
-            style={{ color: "rgba(0,0,0,0.5)", fontSize: "0.875rem" }}
-            className="mb-0"
-          >
-            {tags?.map((tag, i) => (
-              <span key={"tag_" + tag + "_" + i}>
-                <span>{tag}</span>
-                {i + 1 < tags.length && <span> · </span>}
-              </span>
-            ))}
-          </p>
         </div>
         <div className="flex gap-2 w-full h-full overflow-hidden">
-          <div className="w-full flex flex-col justify-between">
+          <div className="w-full flex flex-col justify-between overflow-auto">
             <div>
-              {" "}
               <h5 className="font-semibold text-lg">Inhalt</h5>
-              <p className="text-base">{parsedDescription.inhalt}</p>
+              <p className="text-base text-gray-600">
+                {parsedDescription.inhalt}
+              </p>
               <h5 className="font-semibold text-lg">Sichtbarkeit</h5>
-              <p className="text-base">
+              <p className="text-base text-gray-600">
                 {parsedDescription.sichtbarkeit.slice(0, -1)}
               </p>
               <h5 className="font-semibold text-lg">Nutzung</h5>
-              <p className="text-base">{parsedDescription.nutzung}</p>
+              <p className="text-base text-gray-600">
+                {parsedDescription.nutzung}
+              </p>
             </div>
+            <p
+              style={{ color: "rgba(0,0,0,0.5)", fontSize: "0.875rem" }}
+              className="mb-0"
+            >
+              {tags?.map((tag, i) => (
+                <span key={"tag_" + tag + "_" + i}>
+                  <span>{tag}</span>
+                  {i + 1 < tags.length && <span> · </span>}
+                </span>
+              ))}
+            </p>
           </div>
-          <hr className="h-full w-0.5 my-0 bg-gray-300 rounded-md" />
-          <div className="flex flex-col gap-2 w-1/6">
-            <h5>Links:</h5>
+          <div className="h-full w-0 border-r border-gray-300 my-0" />
+          <div className="flex flex-col gap-0 w-1/4">
+            <h5 className="font-semibold text-lg">Links:</h5>
             <a
               href="https://maps.wuppertal.de/karten?service=WMS&request=GetCapabilities&version=1.1.1"
               target="_blank"
+              className="pb-2"
             >
               GetCapabilities
             </a>
@@ -58,9 +62,9 @@ const InfoCard = ({ layer }: InfoCardProps) => {
               Open Data
             </a>
           </div>
-          <hr className="h-full w-0.5 my-0 bg-gray-300 rounded-md" />
-          <div className="flex flex-col gap-2 w-1/6">
-            <h5>Legende:</h5>
+          <div className="h-full w-0 border-r border-gray-300 my-0" />
+          <div className="flex flex-col gap-0 w-1/4">
+            <h5 className="font-semibold text-lg">Legende:</h5>
             <div className="h-full overflow-auto">
               {legends?.map((legend, i) => (
                 <img
