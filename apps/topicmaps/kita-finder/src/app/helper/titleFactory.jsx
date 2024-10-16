e5c2b94772874b92c123962b944cdbf785eb4658
Thorsten Hell <thorsten.hell@cismet.de>
2024-10-16 12:53:59 +0200

    hardened against undefined array

--- apps/topicmaps/kita-finder/src/app/helper/titleFactory.jsx
+++ apps/topicmaps/kita-finder/src/app/helper/titleFactory.jsx
@@ -1,27 +1,27 @@
-import React from 'react';
+import React from "react";
 const factory = ({ featureCollectionContext }) => {
   const { itemsDictionary, filteredItems, filterState } =
     featureCollectionContext;
   const lebenslagen = itemsDictionary?.lebenslagen || [];
 
-  let themenstadtplanDesc = 'alle Kitas | unter 2 + ab 2 Jahre | 35h pro Woche';
+  let themenstadtplanDesc = "alle Kitas | unter 2 + ab 2 Jahre | 35h pro Woche";
   if (filterState) {
     if (
-      filterState.positiv.length > 0 &&
-      filterState.positiv.length < lebenslagen.length
+      filterState?.positiv?.length > 0 &&
+      filterState?.positiv?.length < lebenslagen.length
     ) {
       if (filterState.positiv.length <= 4) {
-        themenstadtplanDesc += filterState.positiv.join(', ');
+        themenstadtplanDesc += filterState.positiv.join(", ");
       } else {
-        themenstadtplanDesc += filterState.positiv.length + ' Themen';
+        themenstadtplanDesc += filterState.positiv.length + " Themen";
       }
       if (filterState.negativ.length > 0) {
         if (filterState.negativ.length <= 3) {
-          themenstadtplanDesc += ' ohne ';
-          themenstadtplanDesc += filterState.negativ.join(', ');
+          themenstadtplanDesc += " ohne ";
+          themenstadtplanDesc += filterState.negativ.join(", ");
         } else {
           themenstadtplanDesc +=
-            ' (' + filterState.negativ.length + ' Themen ausgeschlossen)';
+            " (" + filterState.negativ.length + " Themen ausgeschlossen)";
         }
       }
     }
